ea34acbca7805556c4165b39f9bf33540bb125bf
Jeremy Walker <jez.walker@gmail.com>
2017-05-17 15:15:15 +0200

    Add auth

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -6,4 +6,14 @@ module LayoutHelper
     classes << "action-#{action_name}"
     classes.join(" ")
   end
+
+  def render_header
+    if devise_controller?
+      render "layouts/logged_out_header"
+    elsif user_signed_in?
+      render "layouts/logged_in_header"
+    else
+      render "layouts/logged_out_header"
+    end
+  end
 end
