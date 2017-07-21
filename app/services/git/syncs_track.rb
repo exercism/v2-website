761f3f8568abc82844ba621d45ac7aed975e66cd
Charles Care <c.p.care@gmail.com>
2017-07-21 20:00:41 +0100

    Hacky update of maintainer and unlocking relationships

--- app/services/git/syncs_track.rb
+++ app/services/git/syncs_track.rb
@@ -8,87 +8,83 @@ class Git::SyncsTrack
 
   def initialize(track)
     @track = track
+    @unlocked_by_relationships = {}
   end
 
   def sync!
-    puts "Syncing #{repo_url}"
-
-    exercises = config[:exercises]
-
     current_exercises_uuids = track.exercises.map { |ex| ex.uuid }
-
-    puts "Current #{current_exercises_uuids}"
-
-    exercises.each.with_index do |exercise, position|
-      exercise_slug = exercise[:slug]
-
-      # Some tracks have old config without keys and others
-      # have new config with keys. Using fetch helps us out
-      # with some sensible defaults where we're on the old config
-
-
-      exercise_uuid = exercise.fetch(:uuid) { SecureRandom.uuid }
-      puts "Syncing exercise #{exercise_uuid}"
-      if current_exercises_uuids.include?(exercise_uuid)
-        ex = Exercise.find_by(uuid: exercise_uuid)
-        ex.update!(slug: exercise_slug,
-          title: exercise_slug.titleize,
-          core: (position <= 10),
-          active: exercise.fetch(:active, true),
-          position: position
-        )
-      else
-        ex = Exercise.new(
-          uuid: exercise.fetch(:uuid) { SecureRandom.uuid },
-          slug: exercise_slug,
-          title: exercise_slug.titleize,
-          core: (position <= 10),
-          active: exercise.fetch(:active, true),
-          position: position
-        )
-        track.exercises << ex
-      end
-    end
-
+    setup_exercises
+    populate_unlocked_by_relationships
   end
 
   private
 
-  def exercise_exists?
+  def populate_unlocked_by_relationships
+    @unlocked_by_relationships.each do |exercise_uuid, unlocked_by_uuid|
+      a = Exercise.find_by(uuid: exercise_uuid)
+      b = Exercise.find_by(uuid: unlocked_by_uuid)
+      a.update!( unlocked_by: b )
+    end
+  end
+
+  def create_or_update_exercise(exercise_slug, exercise_uuid, exercise_data)
+    puts "Syncing exercise #{exercise_uuid}"
+    if current_exercises_uuids.include?(exercise_uuid)
+      ex = Exercise.find_by(uuid: exercise_uuid)
+      ex.update!(exercise_data)
+    else
+      exercise_data.merge!({
+        uuid: exercise_uuid
+      })
+      ex = Exercise.new(exercise_data)
+      track.exercises << ex
+    end
   end
 
   def current_exercises_uuids
-    track.exercises
-  end
-
-  def setup_track
-    if Track.exists?(slug: track_slug)
-      puts "Track #{track_slug} already exists"
-      puts "Update stuff"
-      @track = Track.find_by(slug: track_slug)
-    else
-      @track = CreatesTrack.create!(language, track_slug)
-      puts "New track (id: #{track.id}) created"
-    end
+    track.exercises.map {|ex| ex[:uuid] }
   end
 
   def setup_exercises
     exercises.each.with_index do |exercise, position|
-      sync_exercise(exercise, position)
+      exercise_slug = exercise[:slug]
+      exercise_uuid = exercise[:uuid]
+      puts "UUID: #{exercise_uuid}"
+
+      # HACK!!! TODOGIT this will ultimately be removed
+      exercise_uuid = exercise_slug if exercise_uuid.nil?
+
+      if exercise_slug.nil? || exercise_uuid.nil?
+        puts "Skipping exercise #{exercise}"
+      else
+        sync_exercise(exercise_slug, exercise_uuid, exercise, position)
+      end
     end
   end
 
-  def sync_exercise(exercise, position)
-    puts exercise
-    puts position
-  end
+  def sync_exercise(exercise_slug, exercise_uuid, exercise, position)
+    unlocked_by = exercise[:unlocked_by]
+    @unlocked_by_relationships[exercise_uuid] = unlocked_by unless unlocked_by.nil?
 
-  def track_slug
-    config[:slug]
-  end
+    # Some tracks have old config without keys and others
+    # have new config with keys. Using fetch helps us out
+    # with some sensible defaults where we're on the old config
+    topic_slugs = exercise.fetch(:topics, [])
+    topics = topic_slugs.map { |slug| Topic.find_or_create_by(slug: slug) }
 
-  def language
-    config[:language]
+    exercise_data = {
+      slug: exercise_slug,
+      title: exercise_slug.titleize,
+      core: (position <= 10),
+      active: exercise.fetch(:active, true),
+      position: position,
+      difficulty: exercise.fetch(:difficulty, 1),
+      length: exercise.fetch(:length, 1),
+      topics: topics
+    }
+
+    create_or_update_exercise(exercise_slug, exercise_uuid, exercise_data)
+    sync_maintainers
   end
 
   def exercises
@@ -104,7 +100,31 @@ class Git::SyncsTrack
   end
 
   def repo
-    @repo ||= Git::ExercismRepo.new(repo_url)
+    @repo ||= Git::ExercismRepo.new(repo_url, auto_fetch: true)
+  end
+
+  def sync_maintainers
+    puts "Syncing Maintainers"
+    maintainer_descriptions = config.fetch(:maintainers, [])
+    maintainer_descriptions.each do |m|
+      gh_user = m[:github_username]
+      name = m[:name]
+      next if gh_user.nil? || name.nil?
+      maintainer_data = {
+        active: m.fetch(:active, true),
+        visible: m.fetch(:show_on_website, true),
+        name: m.fetch(:name, '..'),
+        bio: m.fetch(:bio),
+        link_text: m.fetch(:link_text, '..'),
+        link_url: m.fetch(:link_url, '..'),
+        avatar_url: (m[:avatar_url] || 'https://example.com')
+      }
+      puts maintainer_data
+      maintainer = Maintainer.find_or_create_by(track: track, github_username: gh_user) do |mm|
+        mm.assign_attributes(maintainer_data)
+      end
+      maintainer.update(maintainer_data)
+    end
   end
 
 end
