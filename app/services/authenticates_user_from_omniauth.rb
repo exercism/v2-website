d51d02bb8a12fda2c6ff35110fd000097b70554a
Charles Care <c.p.care@gmail.com>
2017-07-26 20:13:01 +0100

    Add TODOs

--- app/services/authenticates_user_from_omniauth.rb
+++ app/services/authenticates_user_from_omniauth.rb
@@ -13,14 +13,14 @@ class AuthenticatesUserFromOmniauth
     user = User.where(provider: auth.provider, uid: auth.uid).first
     return user if user
 
-
-
+    # TODO - catch any omniauth errors and do something sensible (currently you get a 500)
     user = User.create!(
       provider: auth.provider,
       uid: auth.uid,
       email: auth.info.email,
       password: Devise.friendly_token[0,20],
       name: auth.info.name,
+      # TODO - handle nickname/handle clashes
       handle: auth.info.nickname
       #avatar_url: auth.info.image # TODO
     )
