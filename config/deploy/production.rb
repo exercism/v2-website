ba17496c94dfab255b98b27c8d84706fa8eb5542
Charles Care <c.p.care@gmail.com>
2017-07-31 15:27:04 +0100

    Update deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,38 @@
 set :stage, :production
 
-server 'web1.exercism.io', user: fetch(:application), roles: %w{app web db}
-server 'web2.exercism.io', user: fetch(:application), roles: %w{app web db}
+server 'web1.exercism.io', user: fetch(:application), roles: %w{app web db git_fetch}
+server 'web2.exercism.io', user: fetch(:application), roles: %w{app web db git_fetch}
 server 'web3.exercism.io', user: fetch(:application), roles: %w{app web db}
-server 'processor.exercism.io', user: fetch(:application), roles: %w{app web db processor}
+server 'processor.exercism.io', user: fetch(:application), roles: %w{app web db processor git_sync}
 
 namespace :puma_service do
   task :restart do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       execute "sudo /usr/sbin/service #{fetch(:application)} restart"
+    end
+  end
+end
+
+namespace :git_sync do
+  task :restart do
+    on roles(:git_fetch), in: :groups, limit: 3, wait: 10 do
+      execute "sudo /usr/sbin/service exercism_git_fetch restart"
+    end
+    on roles(:git_sync), in: :groups, limit: 3, wait: 10 do
+      execute "sudo /usr/sbin/service exercism_git_sync restart"
+    end
+  end
+end
+
+namespace :sidekiq do
+  task :shutdown do
+    on roles(:processor), in: :groups, limit: 3, wait: 10 do
+      execute "[ -e /opt/exercism/current/tmp/pids/sidekiq.pid ] && kill -TERM `cat /opt/exercism/current/tmp/pids/sidekiq.pid`; exit 0"
+    end
+  end
+  task :restart do
+    on roles(:processor), in: :groups, limit: 3, wait: 10 do
+      execute "sudo /usr/sbin/service exercism-sidekiq restart"
     end
   end
 end
@@ -29,3 +53,4 @@ end
 after "deploy:starting", "sidekiq:shutdown"
 after "deploy:published", "puma_service:restart"
 after "deploy:published", "sidekiq:restart"
+after "deploy:published", "git_sync:restart"
