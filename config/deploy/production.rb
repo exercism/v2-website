99ac1fb8199d828aa2f222fe04b944cb90234fc2
Charles Care <c.p.care@gmail.com>
2017-07-31 13:34:59 +0100

    Switch to multi-server setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,9 @@
 set :stage, :production
 
+server 'web1.exercism.io', user: fetch(:application), roles: %w{app web db}
+server 'web2.exercism.io', user: fetch(:application), roles: %w{app web db}
 server 'web3.exercism.io', user: fetch(:application), roles: %w{app web db}
+server 'processor.exercism.io', user: fetch(:application), roles: %w{app web db processor}
 
 namespace :puma_service do
   task :restart do
@@ -12,18 +15,17 @@ end
 
 namespace :sidekiq do
   task :shutdown do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:processor), in: :groups, limit: 3, wait: 10 do
       execute "kill -TERM `cat /opt/exercism/current/tmp/pids/sidekiq.pid`"
     end
   end
   task :restart do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:processor), in: :groups, limit: 3, wait: 10 do
       execute "sudo /usr/sbin/service exercism-sidekiq restart"
     end
   end
 end
 
 after "deploy:starting", "sidekiq:shutdown"
-
 after "deploy:published", "puma_service:restart"
 after "deploy:published", "sidekiq:restart"
