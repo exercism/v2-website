df99c69cdc5a1abbffe95f4066712a5ef5483b26
Charles Care <c.p.care@gmail.com>
2017-08-03 14:18:30 +0100

    Update Sidekiq config

--- config/initializers/action_job.rb
+++ config/initializers/action_job.rb
@@ -1,4 +1,4 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
-Sidekiq.configure_server do |config|
+Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://processor.exercism.io:6379/0' } if Rails.env.production?
 end
