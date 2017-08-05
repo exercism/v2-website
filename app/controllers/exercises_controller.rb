39bc227d0d7d034f61d9eb163c7577ad300bfaf0
Jeremy Walker <jez.walker@gmail.com>
2017-08-05 23:01:58 +0100

    Make exercises page more colourful

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,6 @@ class ExercisesController < ApplicationController
   def index
     @track = Track.find(params[:track_id])
     @exercises = @track.exercises
-    @solutions = @exercise.solutions.published.includes(:user).limit(6)
 
     return redirect_to [:my, @track] if user_signed_in?
     return redirect_to [@track, @exercise], :status => :moved_permanently if request.path != track_exercises_path(@track)
@@ -11,7 +10,7 @@ class ExercisesController < ApplicationController
   def show
     @track = Track.find(params[:track_id])
     @exercise = @track.exercises.find(params[:id])
-    @solutions = @exercise.solutions.published
+    @solutions = @exercise.solutions.published.includes(:user).limit(6)
 
     return redirect_to [:my, @track] if user_signed_in?
     return redirect_to [@track, @exercise], :status => :moved_permanently if request.path != track_exercise_path(@track, @exercise)
