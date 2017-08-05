39c0f3b429b429eb78c85c942dd523814a94fbba
Jeremy Walker <jez.walker@gmail.com>
2017-08-05 22:43:35 +0100

    Style exercise page

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,9 @@ class ExercisesController < ApplicationController
   def index
     @track = Track.find(params[:track_id])
     @exercises = @track.exercises
+    @solutions = @exercise.solutions.published.includes(:user).limit(6)
 
+    return redirect_to [:my, @track] if user_signed_in?
     return redirect_to [@track, @exercise], :status => :moved_permanently if request.path != track_exercises_path(@track)
   end
 
@@ -11,6 +13,7 @@ class ExercisesController < ApplicationController
     @exercise = @track.exercises.find(params[:id])
     @solutions = @exercise.solutions.published
 
+    return redirect_to [:my, @track] if user_signed_in?
     return redirect_to [@track, @exercise], :status => :moved_permanently if request.path != track_exercise_path(@track, @exercise)
   end
 end
