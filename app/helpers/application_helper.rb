99556e01b4dad2212fb8da9bd7ca3e0ac76267f6
Jeremy Walker <jez.walker@gmail.com>
2017-08-05 17:55:26 +0100

    Refactor icon helper code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,3 @@
 module ApplicationHelper
 
-  def hex_turquoise_icon(track, options={})
-    hex_icon("turquoise", track, options)
-  end
-
-  def hex_white_icon(track, options={})
-    hex_icon("white", track, options)
-  end
-
-  def hex_green_icon(track, options={})
-    hex_icon("green", track, options)
-  end
-
-  def hex_icon(colour, track, options={})
-    options[:onerror] = "this.onerror=null;this.src='https://s3-eu-west-1.amazonaws.com/exercism-static/tracks/default-hex-#{colour}.png'"
-    image_tag(track.send("hex_#{colour}_icon_url"), options)
-  end
-
-  def bordered_green_icon(track, options={})
-    bordered_icon("green", track, options)
-  end
-
-  def bordered_turquoise_icon(track, options={})
-    bordered_icon("turquoise", track, options)
-  end
-
-  def bordered_icon(colour, track, options={})
-    options[:onerror] = "this.onerror=null;this.src='https://s3-eu-west-1.amazonaws.com/exercism-static/tracks/default-bordered-#{colour}.png'"
-    image_tag(track.send("bordered_#{colour}_icon_url"), options)
-  end
-
 end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,36 @@
 module TracksHelper
+
+  def hex_turquoise_track_icon(track, options={})
+    hex_icon("turquoise", track, options)
+  end
+
+  def hex_white_track_icon(track, options={})
+    hex_icon("white", track, options)
+  end
+
+  def hex_green_track_icon(track, options={})
+    hex_icon("green", track, options)
+  end
+
+  def bordered_green_track_icon(track, options={})
+    bordered_icon("green", track, options)
+  end
+
+  def bordered_turquoise_track_icon(track, options={})
+    bordered_icon("turquoise", track, options)
+  end
+
+  def hex_track_icon(colour, track, options={})
+    track_icon(:hex, color, track, options)
+  end
+
+  def bordered_track_icon(colour, track, options={})
+    track_icon(:bordered, color, track, options)
+  end
+
+  def track_icon(type, color, track, options={})
+    options[:onerror] = "this.onerror=null;this.src='https://s3-eu-west-1.amazonaws.com/exercism-static/tracks/default-#{type}-#{colour}.png'"
+    image_tag(track.send("#{type}_#{colour}_icon_url"), options)
+  end
+
 end
