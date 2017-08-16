06c9566001da8960b51c774bba0061d7eb1a0409
Jeremy Walker <jez.walker@gmail.com>
2017-08-16 21:12:56 +0100

    Don't show original exercise files in the UI

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -6,7 +6,18 @@ class Iteration < ApplicationRecord
 
   enum mentor_status: [:pending, :reply, :refactor, :approved]
 
-  #def code
-  #  files.first.file_contents
-  #end
+  def files_to_display
+    @files_to_display ||= begin
+      exercise_slug = solution.exercise.slug
+      track_url = solution.exercise.track.repo_url
+      exercise_reader = Git::ExercismRepo.new(track_url).exercise(exercise_slug)
+      filenames_to_ignore = exercise_reader.files
+
+      fs = []
+      files.each do |file|
+        fs << file unless filenames_to_ignore.include?(file.filename)
+      end
+      fs
+    end
+  end
 end
