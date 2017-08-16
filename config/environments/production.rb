7cf8fddf076edbecfcf4c1679ec0451ec970469d
Jeremy Walker <jez.walker@gmail.com>
2017-08-16 18:09:00 +0100

    Add default route to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options = { :host => "https://exercism.io" }
