428278d6638ca013b9adae1a77c34f6d3f97dd88
Jeremy Walker <jez.walker@gmail.com>
2017-08-17 14:51:25 +0100

    Fix repo url in config

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.8.2"
 @application = "exercism"
 
 set :application, @application
-set :repo_url, "git@github.com:exercism/prototyping.git"
+set :repo_url, "git@github.com:exercism/prototype.git"
 set :branch, 'master'
 
 set :linked_files, %w{config/database.yml config/auth.yml config/smtp.yml}
