a78f92bab5abb5f649e8a1ab48aedfbd0be469c8
Jeremy Walker <jez.walker@gmail.com>
2017-08-17 15:38:19 +0100

    Tidy up from ceasing to filter files

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -5,21 +5,4 @@ class Iteration < ApplicationRecord
   has_many :notifications, as: :about
 
   enum mentor_status: [:pending, :reply, :refactor, :approved]
-
-  def files_to_display
-    return files
-
-    @files_to_display ||= begin
-      exercise_slug = solution.exercise.slug
-      track_url = solution.exercise.track.repo_url
-      exercise_reader = solution.exercise.track.repo.exercise(exercise_slug)
-      filenames_to_ignore = exercise_reader.files
-
-      fs = []
-      files.each do |file|
-        fs << file unless filenames_to_ignore.include?(file.filename)
-      end
-      fs
-    end
-  end
 end
