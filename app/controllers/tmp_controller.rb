46b02667ebc2d9f49a708e4e9ee68a854e354ae6
Jeremy Walker <jez.walker@gmail.com>
2017-08-19 16:22:46 +0100

    Update tmp controller to add a single clearer file.

--- app/controllers/tmp_controller.rb
+++ app/controllers/tmp_controller.rb
@@ -2,14 +2,13 @@ class TmpController < ApplicationController
   def create_iteration
     @solution = current_user.solutions.find_by_uuid!(params[:solution_id])
 
-    files = (rand(2) + 1).times.map {
-      tempfile = Tempfile.new
-      tempfile.write("foobar")
-      file = ActionDispatch::Http::UploadedFile.new(tempfile: tempfile, type: 'text/plain')
-      file.headers = "Content-Disposition: form-data; name=\"files[]\"; filename=\"/lib/three.py\"\r\nContent-Type: application/octet-stream\r\n"
-      file
-    }
-    CreatesIteration.create!(@solution, files)
+    tempfile = Tempfile.new
+    tempfile.write("A fake file that was created when you clicked the 'create fake iteration' button.")
+    tempfile.rewind
+    file = ActionDispatch::Http::UploadedFile.new(tempfile: tempfile, type: 'text/plain')
+    file.headers = "Content-Disposition: form-data; name=\"files[]\"; filename=\"fake-solution-created-with-button.txt\"\r\nContent-Type: application/octet-stream\r\n"
+
+    CreatesIteration.create!(@solution, [file])
     redirect_to [:my, @solution]
   end
 end
