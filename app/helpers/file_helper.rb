a1520d77a2b5dd9d15e24e14cdc30725a2f5bcb3
Jeremy Walker <jez.walker@gmail.com>
2017-08-24 19:04:57 +0100

    Extra file formats out into config and readd line numbers

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -2,19 +2,8 @@ module FileHelper
   def syntax_highlighter_for_filename(filename, track)
     parts = filename.split(".")
     ext = parts.size == 1 ? nil : parts.last
-    p ext
 
-    lang = case ext
-      when "rb"
-        "ruby"
-      when "go"
-        "go"
-      when "txt"
-        "plain"
-      else
-        track.syntax_highligher_language
-      end
-
+    lang = Exercism::PrismFileMappings[ext]
     "language-#{lang}"
   end
 end
