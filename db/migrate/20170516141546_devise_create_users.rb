953822c124e162fde2b68143f56d0c3425b3857b
Jeremy Walker <jez.walker@gmail.com>
2017-08-25 22:57:51 +0100

    Flatten migrations

--- db/migrate/20170516141546_devise_create_users.rb
+++ db/migrate/20170516141546_devise_create_users.rb
@@ -45,6 +45,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :handle,                unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/migrate/20170824162220_add_legacy_to_solutions.rb
+++
@@ -1,5 +0,0 @@
-class AddLegacyToSolutions < ActiveRecord::Migration[5.1]
-  def change
-    add_column :solutions, :is_legacy, :boolean, null: false, default: false
-  end
-end
