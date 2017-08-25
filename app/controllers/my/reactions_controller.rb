a8cf5da1234b3a357a88ad95337452fee7909209
Jeremy Walker <jez.walker@gmail.com>
2017-08-25 14:19:14 +0100

    Add unselectable reactions

--- app/controllers/my/reactions_controller.rb
+++ app/controllers/my/reactions_controller.rb
@@ -9,11 +9,16 @@ class My::ReactionsController < MyController
 
   def create
     @solution = Solution.published.find_by_uuid!(params[:solution_id])
-    data = { emotion: params[:emotion] }
-    @reaction = Reaction.find_or_create_by(user: current_user, solution: @solution) do |ur|
-      ur.attributes = data
+    @reaction = Reaction.where(user: current_user, solution: @solution).first
+    if @reaction
+      if params[:emotion] == @reaction.emotion
+        @reaction.destroy unless @reaction.comment.present?
+      else
+        @reaction.update(emotion: params[:emotion])
+      end
+    else
+      @reaction = Reaction.create(user: current_user, solution: @solution, emotion: params[:emotion])
     end
-    @reaction.update(data)
     @comments = @solution.reactions.with_comments.includes(user: :profile)
     @reaction_counts = @solution.reactions.group(:emotion).count.to_h
   end
