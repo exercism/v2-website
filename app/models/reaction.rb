caaf9443b0e895039d9f9dff7b70ede8b7400a5f
Jeremy Walker <jez.walker@gmail.com>
2017-08-26 20:51:26 +0100

    Add reaction counts to solutions

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,7 +2,7 @@ class Reaction < ApplicationRecord
   belongs_to :solution
   belongs_to :user
 
-  enum emotion: [ :like, :love, :genius ]
+  enum emotion: [ :like, :love, :genius, :legacy ]
 
   scope :with_comments, -> { where("comment != '' AND comment IS NOT NULL") }
 end
--- db/migrate/20170516141759_create_solutions.rb
+++ db/migrate/20170516141759_create_solutions.rb
@@ -15,6 +15,7 @@ class CreateSolutions < ActiveRecord::Migration[5.1]
       t.datetime :last_updated_by_user_at, null: true
       t.datetime :last_updated_by_mentor_at, null: true
       t.integer :num_mentors, null: false, default: 0
+      t.integer :num_reactions, null: false, default: 0
 
       t.text :reflection, null: true
 
