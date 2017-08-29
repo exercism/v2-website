dc93bd16e5974518a4aa6bfce9b16d87cbbe81ba
Karlo Soriano <karlo@thalamus.ai>
2017-08-29 13:29:15 +0800

    Add RepoUpdate#repo to return repo to fetch

--- app/models/repo_update.rb
+++ app/models/repo_update.rb
@@ -1,3 +1,14 @@
 class RepoUpdate < ApplicationRecord
   validates :slug, presence: true
+
+  def repo
+    case slug
+    when "problem-specifications"
+      Git::ProblemSpecifications.head
+    when "website-copy"
+      Git::WebsiteContent.head
+    else
+      Track.find_by!(slug: slug).repo
+    end
+  end
 end
--- test/models/repo_update_test.rb
+++ test/models/repo_update_test.rb
@@ -10,4 +10,25 @@ class RepoUpdateTest < ActiveSupport::TestCase
 
     refute repo_update.valid?
   end
+
+  test "repo returns a matching track's repo" do
+    repo_update = create(:repo_update, slug: "go")
+    track = create(:track,
+                   slug: "go",
+                   repo_url: "https://github.com/exercism/go")
+
+    assert_equal track.repo, repo_update.repo
+  end
+
+  test "repo returns the problem specs repo when slug matches" do
+    repo_update = create(:repo_update, slug: "problem-specifications")
+
+    assert_equal Git::ProblemSpecifications.head, repo_update.repo
+  end
+
+  test "repo returns the website copy repo when slug matches" do
+    repo_update = create(:repo_update, slug: "website-copy")
+
+    assert_equal Git::WebsiteContent.head, repo_update.repo
+  end
 end
