429cf1d21858e11ce7b50ee4056cecb73dfa529d
Karlo Soriano <karlo@thalamus.ai>
2017-09-06 07:37:32 +0800

    Fetch problem specs before syncing

--- test/services/git/syncs_tracks_test.rb
+++ test/services/git/syncs_tracks_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class Git::SyncsTracksTest < ActiveSupport::TestCase
+  test "fetches problem specs" do
+    repo = mock()
+    repo.expects(:fetch!)
+    Git::ProblemSpecifications.stubs(:head).returns(repo)
+
+    Git::SyncsTracks.sync([])
+  end
+
   test "syncs tracks" do
+    Git::ProblemSpecifications.stubs(:head).returns(stub(:fetch!))
     track = create(:track)
 
     Git::SyncsTrack.expects(:sync!).with(track)
