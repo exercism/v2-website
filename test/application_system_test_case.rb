db5aff4bb679fed717db1d5ae82d66d540156949
Karlo Soriano <karlo@thalamus.ai>
2017-09-21 10:26:15 +0800

    Add Devise helpers to ApplicationSystemTestCase

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  protected
+
+  def sign_in!(user = nil)
+    @current_user = user || create(:user)
+    @current_user.confirm
+    sign_in @current_user
+  end
 end
