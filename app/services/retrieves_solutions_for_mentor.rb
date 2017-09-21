91d04d419abfe294e50b1a218407bea2311b69af
Karlo Soriano <karlo@thalamus.ai>
2017-09-21 23:12:45 +0800

    Refactor retrieving service objects to allow for multiple filters

---
+++ app/services/retrieves_solutions_for_mentor.rb
@@ -0,0 +1,24 @@
+class RetrievesSolutionsForMentor
+  def self.retrieve(*args)
+    new(*args).retrieve
+  end
+
+  def initialize(mentor, filter)
+    @solutions = mentor.mentored_solutions
+    @filter = filter
+  end
+
+  def retrieve
+    filter_by_status! if filter
+
+    solutions
+  end
+
+  private
+  attr_reader :solutions, :filter
+
+  def filter_by_status!
+    @solutions = FiltersSolutionsByStatus.filter(solutions, filter)
+  end
+
+end
