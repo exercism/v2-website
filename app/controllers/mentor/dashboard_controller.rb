895a5f0f5527d7abd3e23a4e5fd7515f7e5027ed
Karlo Soriano <karlo@thalamus.ai>
2017-09-22 15:12:11 +0800

    Refactor controllers and responses for readability

--- app/controllers/mentor/dashboard_controller.rb
+++ app/controllers/mentor/dashboard_controller.rb
@@ -2,21 +2,14 @@ class Mentor::DashboardController < MentorController
   def show
     @status = params[:status]
     @status_options = SolutionMentorship::STATUSES
+
     @track_id = params[:track_id]
-    @track_id_options = current_user.
-      mentored_tracks.
-      map { |track| [track.title, track.id] }
+    @track_id_options = OptionsHelper.as_options(current_user.mentored_tracks,
+                                                 :title,
+                                                 :id)
+
     @exercise_id = params[:exercise_id]
-    @track = Track.find_by(id: @track_id)
-    @exercise_id_options = if @track
-                             @track.
-                               exercises.
-                               map do |exercise|
-                                 { text: exercise.title, value: exercise.id }
-                               end
-                           else
-                             []
-                           end
+    @exercise_id_options = exercise_id_options
 
     @your_solutions = RetrievesSolutionsForMentor.retrieve(
       current_user,
@@ -33,6 +26,15 @@ class Mentor::DashboardController < MentorController
     @total_solutions_count = current_user.mentored_solutions.count
     @total_students_count = current_user.mentored_solutions.select(:user_id).distinct.count
     @feedbacks = current_user.solution_mentorships.where(show_feedback_to_mentor: true).order('updated_at desc').limit(5).pluck(:feedback)
+  end
+
+  private
+
+  def exercise_id_options
+    track = Track.find_by(id: @track_id)
+    return [] unless track
+
+    OptionsHelper.as_options(track.exercises, :title, :id)
   end
 end
 
