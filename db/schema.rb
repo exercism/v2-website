24e7e1cf2685bff21334207087f9a87cbd1f6cef
Katrina Owen <kytrinyx@github.com>
2017-09-23 15:15:36 -0600

    Add default value to contributors.num_migrations
    We will be incrementing the num_migrations upon receipt of a webhook. If this value is NULL then
    we need to have extra logic to handle either NULL or a number. By giving this a value we can
    simplify the logic.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923144117) do
+ActiveRecord::Schema.define(version: 20170923211039) do
 
   create_table "auth_tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.bigint "user_id", null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170923144117) do
   create_table "contributors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "github_username", null: false
     t.string "avatar_url", null: false
-    t.integer "num_contributions", null: false
+    t.integer "num_contributions", default: 0, null: false
     t.boolean "is_maintainer", default: false, null: false
     t.boolean "is_core", default: false, null: false
     t.datetime "created_at", null: false
