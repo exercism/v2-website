bf4f66eb76444490bc7e5e3582a11b3843dcfac2
Katrina Owen <kytrinyx@github.com>
2017-09-23 17:29:02 -0600

    Handle contribution logic in contributor
    The contributor should be able to absorb contribution information and give back a (presumably)
    valid object to be saved.

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,3 +1,11 @@
 class Contributor < ApplicationRecord
   validates_presence_of :github_id, :github_username
+
+  def self.with_contribution(contribution)
+    contributor = find_or_initialize_by(github_id: contribution.github_id)
+    contributor.avatar_url = contribution.avatar_url
+    contributor.github_username = contribution.username
+    contributor.num_contributions += 1
+    contributor
+  end
 end
