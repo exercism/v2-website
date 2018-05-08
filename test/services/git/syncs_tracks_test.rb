879e2ec7eadcdbc6420b4fb0c28017e015f9ba60
Katrina Owen <kytrinyx@github.com>
2018-05-08 06:59:34 +0200

    Override output streams in SyncsTracks tests
    Use a StringIO object to capture the output. We aren't actually
    checking anything, so these are just being injected directly.

--- test/services/git/syncs_tracks_test.rb
+++ test/services/git/syncs_tracks_test.rb
@@ -6,7 +6,7 @@ class Git::SyncsTracksTest < ActiveSupport::TestCase
     repo.expects(:fetch!)
     Git::ProblemSpecifications.stubs(:head).returns(repo)
 
-    Git::SyncsTracks.sync([])
+    Git::SyncsTracks.sync([], stdout: StringIO.new, stderr: StringIO.new)
   end
 
   test "syncs tracks" do
@@ -15,14 +15,14 @@ class Git::SyncsTracksTest < ActiveSupport::TestCase
 
     Git::SyncsTrack.expects(:sync!).with(track)
 
-    Git::SyncsTracks.sync([track])
+    Git::SyncsTracks.sync([track], stdout: StringIO.new, stderr: StringIO.new)
   end
 
   test "approves hello world exercises" do
     Git::ProblemSpecifications.stubs(:head).returns(stub(:fetch!))
     exercise = create(:exercise, slug: "hello-world", auto_approve: false)
 
-    Git::SyncsTracks.sync([])
+    Git::SyncsTracks.sync([], stdout: StringIO.new, stderr: StringIO.new)
 
     exercise.reload
 
