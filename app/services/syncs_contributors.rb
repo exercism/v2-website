63e383023e552834013715559827364cd6b2e06d
Jeremy Walker <jez.walker@gmail.com>
2018-05-30 17:24:46 +0100

    Add error handling to contributors

--- app/services/syncs_contributors.rb
+++ app/services/syncs_contributors.rb
@@ -11,15 +11,19 @@ class SyncsContributors
   def sync!
     contributors = JSON.parse(File.read(filepath), symbolize_names: true)
     contributors.each do |_, contributor_data|
-      data = {
-        avatar_url: contributor_data[:avatar],
-        num_contributions: contributor_data[:repositories].sum{|r|r[:contributions]},
-      }
-      username = contributor_data[:username]
-      contributor = Contributor.find_or_create_by!(github_username: username) do |c|
-        c.assign_attributes(data)
+      begin
+        data = {
+          avatar_url: contributor_data[:avatar],
+          num_contributions: contributor_data[:repositories].sum{|r|r[:contributions]},
+        }
+        username = contributor_data[:username]
+        contributor = Contributor.find_or_create_by!(github_username: username) do |c|
+          c.assign_attributes(data)
+        end
+        contributor.update!(data)
+      rescue
+        puts "Failed to processed #{contributor_data}"
       end
-      contributor.update!(data)
     end
 
     Contributor.where(github_username: Maintainer.select(:github_username)).update_all(is_maintainer: true)
