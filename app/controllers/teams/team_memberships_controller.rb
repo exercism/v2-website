8978238dc76dc0e36499b1bb55baa9da1b63937c
Karlo Soriano <karlo@thalamus.ai>
2018-06-14 19:07:02 +0800

    Prevent non-admins from removing team members

--- app/controllers/teams/team_memberships_controller.rb
+++ app/controllers/teams/team_memberships_controller.rb
@@ -2,10 +2,16 @@ class Teams::TeamMembershipsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    team_membership = TeamMembership.find(params[:id])
+    team_membership = teams_managed_members.find(params[:id])
 
     team_membership.destroy
 
     redirect_to teams_team_memberships_path(team_membership.team)
+  end
+
+  private
+
+  def teams_managed_members
+    TeamMembership.where(team_id: current_user.teams_managed.pluck(:id))
   end
 end
