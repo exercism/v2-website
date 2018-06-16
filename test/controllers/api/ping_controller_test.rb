b321e51f45362c16d93e234904374c39e5a194e1
Jeremy Walker <jez.walker@gmail.com>
2018-06-16 18:23:30 +0100

    Add validate_token API endpoint

--- test/controllers/api/ping_controller_test.rb
+++ test/controllers/api/ping_controller_test.rb
@@ -1,7 +1,7 @@
 require_relative './test_base'
 
 class API::PingControllerTest < API::TestBase
-  test "latest should return 401 with incorrect token" do
+  test "should return 200" do
     get api_ping_path, as: :json
     assert_response 200
   end
