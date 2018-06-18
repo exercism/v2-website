cf15efb11cc91a272234868b07824561f6b9035a
Karlo Soriano <karlo@thalamus.ai>
2018-06-18 20:09:18 +0800

    Remove unneeded indirection

--- app/controllers/teams/team_memberships_controller.rb
+++ app/controllers/teams/team_memberships_controller.rb
@@ -2,16 +2,12 @@ class Teams::TeamMembershipsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    team_membership = teams_managed_members.find(params[:id])
+    team_membership = TeamMembership.
+      where(team_id: current_user.teams_managed.pluck(:id)).
+      find(params[:id])
 
     team_membership.destroy
 
     redirect_to teams_team_memberships_path(team_membership.team)
-  end
-
-  private
-
-  def teams_managed_members
-    TeamMembership.where(team_id: current_user.teams_managed.pluck(:id))
   end
 end
