f9743a09329abbbbde353befbeee01c81f4090b9
Karlo Soriano <karlo@thalamus.ai>
2018-06-19 14:00:03 +0800

    Add test to check that non-admins aren't allowed to delete invites

--- test/controllers/teams/invitations_controller_test.rb
+++ test/controllers/teams/invitations_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class Teams::InvitationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "prevents non-admins from removing invites" do
+    non_admin = create(:user)
+    team = create(:team)
+    create(:team_membership,
+           user: non_admin,
+           team: team,
+           admin: false)
+    invite = create(:team_invitation, team: team)
+
+    sign_in!(non_admin)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      delete teams_invitation_url(invite)
+    end
+  end
 end
