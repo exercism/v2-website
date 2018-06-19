c7dcb2666e6e16f0b72681b0e721cbf056c21590
Jeremy Walker <jez.walker@gmail.com>
2018-06-19 11:12:14 +0100

    Fix the build

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -8,14 +8,16 @@ class ProfileTest < ActiveSupport::TestCase
     good_user_track = create :user_track, anonymous: false, user: user
     bad_user_track = create :user_track, anonymous: true, user: user
 
-    good_exercise = create :exercise, track: good_user_track.track
+    good_exercise_1 = create :exercise, track: good_user_track.track
+    good_exercise_2 = create :exercise, track: good_user_track.track
+    good_exercise_3 = create :exercise, track: good_user_track.track
     bad_exercise = create :exercise, track: bad_user_track.track
 
     create :solution, published_at: nil, user: user
     create :solution, published_at: DateTime.now, exercise: bad_exercise, user: user
-    s1 = create :solution, published_at: DateTime.now, exercise: good_exercise, user: user, num_reactions: 2
-    s2 = create :solution, published_at: DateTime.now, exercise: good_exercise, user: user, num_reactions: 1
-    s3 = create :solution, published_at: DateTime.now, exercise: good_exercise, user: user, num_reactions: 3
+    s1 = create :solution, published_at: DateTime.now, exercise: good_exercise_1, user: user, num_reactions: 2
+    s2 = create :solution, published_at: DateTime.now, exercise: good_exercise_2, user: user, num_reactions: 1
+    s3 = create :solution, published_at: DateTime.now, exercise: good_exercise_3, user: user, num_reactions: 3
 
     assert_equal [s3,s1,s2], profile.solutions
   end
