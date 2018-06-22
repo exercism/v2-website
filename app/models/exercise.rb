6fc1ab33917be723eac7e2e1a5edaa1dae620b2f
Jeremy Walker <jez.walker@gmail.com>
2018-06-22 17:00:23 +0100

    Use the mentorcism command

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -27,7 +27,7 @@ class Exercise < ApplicationRecord
 
   # BETA
   def download_command
-    "nextercism download #{slug} --track=#{track.slug}"
+    "mentorcism download #{slug} --track=#{track.slug}"
   end
 
   def side?
