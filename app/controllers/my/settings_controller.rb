9e1f8747119e606decb088b6372c6184d7a76389
Jeremy Walker <jez.walker@gmail.com>
2018-06-29 11:47:13 +0100

    Add ability to change handle in settings

--- app/controllers/my/settings_controller.rb
+++ app/controllers/my/settings_controller.rb
@@ -33,25 +33,45 @@ class My::SettingsController < MyController
 
   def update
     if params[:user][:password].present?
-      if current_user.provider? || current_user.valid_password?(params[:old_password])
-        if current_user.update(
-            password: params[:user][:password],
-            password_confirmation: params[:user][:password_confirmation]
-          )
-          sign_in(current_user, bypass: true)
-          flash.notice = "Password updated successfully"
-        else
-          setup_show
-          return render action: :show
-        end
-      else
-        flash.alert = "Password updated successfully"
-      end
+      return unless update_password
+    elsif params[:user][:handle].present?
+      return unless update_handle
     end
     redirect_to action: :show
   end
 
   private
+
+  def update_password
+    if !current_user.provider? && !current_user.valid_password?(params[:old_password])
+      flash.alert = "Password updated successfully"
+      return true
+    end
+
+    if current_user.update(
+        password: params[:user][:password],
+        password_confirmation: params[:user][:password_confirmation]
+      )
+      sign_in(current_user, bypass: true)
+      flash.notice = "Password updated successfully"
+      return true
+    else
+      setup_show
+      render action: :show
+      return false
+    end
+  end
+
+  def update_handle
+    if current_user.update(handle: params[:user][:handle])
+      flash.notice = "Handle updated successfully"
+      return true
+    else
+      setup_show
+      render action: :show
+      return false
+    end
+  end
 
   def setup_show
     @user_tracks = current_user.user_tracks.includes(:track)
