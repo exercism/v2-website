0050547c2dfbaae0ba8df1a6634335fbf01b58af
Karlo Soriano <karlo@thalamus.ai>
2018-07-10 10:03:12 +0100

    Preserve session across subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: '_exercism_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_exercism_session', domain: :all, tld_length: 2
 #Rails.application.config.session_store :cookie_store, key: '_exercism_session', domain: {
 #  production: '.exercism.io',
 #  development: '.localhost:3000'
