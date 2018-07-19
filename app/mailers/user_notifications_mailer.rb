5e84e18608b013c95b7fc20941cea72933d36595
Jeremy Walker <jez.walker@gmail.com>
2018-07-19 13:50:08 +0100

    Add more info to mailer subject lines

--- app/mailers/user_notifications_mailer.rb
+++ app/mailers/user_notifications_mailer.rb
@@ -3,9 +3,11 @@ class UserNotificationsMailer < ApplicationMailer
     @user = user
     @discussion_post = discussion_post
     @solution = discussion_post.solution
+    exercise_title = @solution.exercise.title
+    track_title = @solution.track.title
     mail(
       to: @user.email,
-      subject: "[Exercism] A mentor has commented on your solution"
+      subject: "[Exercism] A mentor has commented on your solution to #{track_title}/#{exercise_title}"
     )
   end
 end
--- test/mailers/notifications_mailer_test.rb
+++ test/mailers/notifications_mailer_test.rb
@@ -6,6 +6,10 @@ class NotificationsMailerTest < ActionMailer::TestCase
     user = discussion_post.iteration.solution.user
     mentor = discussion_post.user
 
+    solution = discussion_post.solution
+    exercise_title = solution.exercise.title
+    track_title = solution.track.title
+
     email = UserNotificationsMailer.new_discussion_post(user, discussion_post)
 
     assert_emails 1 do
@@ -14,6 +18,6 @@ class NotificationsMailerTest < ActionMailer::TestCase
 
     assert_equal ["hello@exercism.io"], email.from
     assert_equal [user.email], email.to
-    assert_equal '[Exercism] A mentor has commented on your solution', email.subject
+    assert_equal "[Exercism] A mentor has commented on your solution to #{track_title}/#{exercise_title}", email.subject
   end
 end
