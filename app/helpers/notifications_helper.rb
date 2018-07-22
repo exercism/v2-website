03ec7555b02f98b5eb473e269256204190ac1a49
Jeremy Walker <jez.walker@gmail.com>
2018-07-22 16:36:12 +0200

    Add some clarity and extra cases

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,10 +2,33 @@ module NotificationsHelper
   def notification_image(notification)
     case notification.type
     when 'new_discussion_post'
+      # Face of the discussion post user
+      # trigger = discussion post
       notification.trigger.user.avatar_url
+
     when 'new_discussion_post_for_mentor'
+      # Face of the discussion post user
+      # trigger = discussion post
       notification.trigger.user.avatar_url
+
     when 'new_iteration_for_mentor'
+      # Face of the iteration user
+      # about = solution
+      notification.about.user.avatar_url
+
+    when 'new_reaction'
+      # Face of the reacting user
+      # trigger = reaction
+      notification.trigger.user.avatar_url
+
+    when 'exercise_auto_approved'
+      # Your own face
+      # about = solution
+      notification.about.user.avatar_url
+
+    when 'solution_approved'
+      # Your own face
+      # about = solution
       notification.about.user.avatar_url
     end
   end
