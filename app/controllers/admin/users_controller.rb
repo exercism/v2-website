84cb61e5c0ec91ff624b724acb20401036bf81b3
Jeremy Walker <jez.walker@gmail.com>
2018-07-23 17:36:25 +0200

    Tweak admin users searching and ordering

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,13 @@
 class Admin::UsersController < AdminController
   def show
-    @user = User.find(params[:id])
-    @discussion_posts = @user.discussion_posts.includes(iteration: :solution)
+    if params[:id].present?
+      @user = User.find_by_id(params[:id])
+    elsif params[:handle].present?
+      @user = User.find_by_handle(params[:handle])
+      @user = UserTrack.find_by_handle(params[:handle]).user unless @user
+    end
+
+    @discussion_posts = @user.discussion_posts.includes(iteration: :solution).
+                                               order('id desc')
   end
 end
