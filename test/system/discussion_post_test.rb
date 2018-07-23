15acfe2ef8a0d90777f177f6b1a896eae123d21a
Karlo Soriano <karlo@thalamus.ai>
2018-07-23 19:19:56 -0400

    Allow editing mentor comments in place

--- test/system/discussion_post_test.rb
+++ test/system/discussion_post_test.rb
@@ -32,6 +32,34 @@ class DiscussionPostTest < ApplicationSystemTestCase
     assert_equal "Hello!", discussion_post.previous_content
   end
 
+  test "mentor edits a discussion post" do
+    mentor = create(:user,
+                    accepted_terms_at: Date.new(2016, 12, 25),
+                    accepted_privacy_policy_at: Date.new(2016, 12, 25))
+    track = create(:track)
+    create(:track_mentorship, track: track, user: mentor)
+    exercise = create(:exercise, track: track)
+    solution = create(:solution, exercise: exercise)
+    iteration = create(:iteration, solution: solution)
+    create(:solution_mentorship, user: mentor, solution: solution)
+    discussion_post = create(:discussion_post,
+                             content: "Hello!",
+                             html: "<p>Hello!</p>",
+                             user: mentor,
+                             iteration: iteration)
+
+    sign_in!(mentor)
+    visit mentor_solution_path(solution)
+    click_on "Edit"
+    fill_simple_mde "discussion_post_content", "Hey!"
+    click_on "Update"
+
+    assert_text "Hey!"
+    assert_text "(edited)"
+    discussion_post.reload
+    assert_equal "Hello!", discussion_post.previous_content
+  end
+
   private
 
   def fill_simple_mde(id, value)
