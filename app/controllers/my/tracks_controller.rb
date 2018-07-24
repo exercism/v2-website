ea5484d1fa7edef485dedc42630c76dc9731d461
Karlo Soriano <karlo@thalamus.ai>
2018-07-23 21:29:09 -0400

    Show the correct page when rejoining a track

--- app/controllers/my/tracks_controller.rb
+++ app/controllers/my/tracks_controller.rb
@@ -17,6 +17,7 @@ class My::TracksController < MyController
     @track = Track.find(params[:id])
     return redirect_to @track unless user_signed_in?
     return show_not_joined unless current_user.joined_track?(@track)
+    return show_not_joined if current_user.previously_joined_track?(@track)
     solutions = current_user.solutions.includes(:exercise).where('exercises.track_id': @track.id)
     mapped_solutions = solutions.each_with_object({}) {|s,h| h[s.exercise_id] = s }
 
