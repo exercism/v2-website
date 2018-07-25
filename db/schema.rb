19e1cb8bcd44764ea9ade84eeacff5bac5b94add
Jeremy Walker <jez.walker@gmail.com>
2018-07-25 13:59:57 +0200

    Optimise mentor solution selection speed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_105321) do
+ActiveRecord::Schema.define(version: 2018_07_25_115010) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -277,6 +277,7 @@ ActiveRecord::Schema.define(version: 2018_07_25_105321) do
     t.index ["last_updated_by_user_at"], name: "ihid-3"
     t.index ["num_mentors", "exercise_id", "user_id"], name: "fix-4"
     t.index ["num_mentors", "exercise_id"], name: "fix-2"
+    t.index ["num_mentors", "independent_mode", "created_at", "exercise_id"], name: "mentor_selection_idx_1"
     t.index ["num_mentors", "last_updated_by_user_at"], name: "ihid-4"
     t.index ["num_mentors", "user_id", "exercise_id"], name: "fix-5"
     t.index ["num_mentors", "user_id"], name: "fix-3"
