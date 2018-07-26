1a5d687d99a73a5b18f40c80c8f09b48fbb96e4f
Douglas F Shearer <dougal.s@gmail.com>
2018-07-26 13:37:46 +0100

    Use headless Chrome for system tests.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,7 +7,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include SelectizeHelpers
   include StubRepoCache
 
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   protected
 
