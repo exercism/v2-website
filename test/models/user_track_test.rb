779cb3d67f98324031a5991580ac9de40a48c5f9
Jeremy Walker <jez.walker@gmail.com>
2018-07-26 11:04:07 +0200

    Add methods for to measure user_track usage

--- test/models/user_track_test.rb
+++ test/models/user_track_test.rb
@@ -56,4 +56,30 @@ class UserTrackTest < ActiveSupport::TestCase
 
     assert_equal [archived], UserTrack.archived
   end
+
+  test "solutions_being_mentored" do
+    user = create :user
+    track = create :track
+    user_track = create :user_track, user: user, track: track
+
+    assert_equal 0, user_track.num_solutions_being_mentored
+    assert_equal 1, user_track.mentoring_slots_remaining
+    assert user_track.mentoring_slots_remaining
+
+    s1 = create :solution, independent_mode: true, mentoring_enabled: true, user: user, exercise: create(:exercise, track: track)
+         create :solution, independent_mode: true, mentoring_enabled: true, approved_by: create(:user), user: user, exercise: create(:exercise, track: track)
+         create :solution, independent_mode: true, mentoring_enabled: false, user: user, exercise: create(:exercise, track: track)
+         create :solution, independent_mode: true, mentoring_enabled: false, approved_by: create(:user), user: user, exercise: create(:exercise, track: track)
+    s2 = create :solution, independent_mode: false, mentoring_enabled: true, user: user, exercise: create(:exercise, track: track)
+         create :solution, independent_mode: false, mentoring_enabled: true, approved_by: create(:user), user: user, exercise: create(:exercise, track: track)
+    s3 = create :solution, independent_mode: false, mentoring_enabled: false, user: user, exercise: create(:exercise, track: track)
+         create :solution, independent_mode: false, mentoring_enabled: false, approved_by: create(:user), user: user, exercise: create(:exercise, track: track)
+
+    expected = [s1,s2,s3].sort.map(&:id)
+    assert_equal expected, user_track.solutions_being_mentored.sort.map(&:id)
+    assert_equal 3, user_track.num_solutions_being_mentored
+    assert_equal -2, user_track.mentoring_slots_remaining
+    refute user_track.mentoring_slots_remaining?
+  end
+
 end
