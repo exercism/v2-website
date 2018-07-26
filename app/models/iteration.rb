4e371ec5a4e8decdb3be9a8d13c42ebfa8719e2f
Jeremy Walker <jez.walker@gmail.com>
2018-07-26 11:28:09 +0200

    Add methods for subsets of notifications and discussion posts

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -3,4 +3,12 @@ class Iteration < ApplicationRecord
   has_many :files, class_name: "IterationFile", dependent: :destroy
   has_many :discussion_posts, dependent: :destroy
   has_many :notifications, as: :about
+
+  def discussion_post_notifications
+    notifications.where(trigger_type: "DiscussionPost")
+  end
+
+  def mentor_discussion_posts
+    discussion_posts.where.not(user_id: solution.user_id)
+  end
 end
