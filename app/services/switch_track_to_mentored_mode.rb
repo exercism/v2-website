0b88d5079ec412c95f962703c7169b63d4752cc1
Jeremy Walker <jez.walker@gmail.com>
2018-07-27 11:37:47 +0200

    Improve logic around switching back to independent mode

--- app/services/switch_track_to_mentored_mode.rb
+++ app/services/switch_track_to_mentored_mode.rb
@@ -9,8 +9,17 @@ class SwitchTrackToMentoredMode
 
   def call
     user_track.update(independent_mode: false)
-    user_track.solutions.not_completed.update_all(independent_mode: false)
     user_track.solutions.update_all(track_in_independent_mode: false)
+
+    # Delete all unsubmitted exercises
+    user_track.solutions.not_started.destroy_all
+
+    # Get the core exercises
+    exercise_ids = Exercise.core.where(id: user_track.solutions.completed.map(&:exercise_id)).pluck(:id)
+    Exercise.where(unlocked_by: exercise_ids).each { |e|CreateSolution.(user, e) }
+    UnlocksNextCoreExercise.(track, user)
+
+    user_track.solutions.not_completed.update_all(independent_mode: false)
   end
 
   memoize
--- test/services/switch_track_to_mentored_mode_test.rb
+++ test/services/switch_track_to_mentored_mode_test.rb
@@ -6,6 +6,8 @@ class SwitchTrackToMentoredModeTest < ActiveSupport::TestCase
     track = create :track
     completed_solution = create :solution, user: user, exercise: create(:exercise, track: track), independent_mode: true, completed_at: DateTime.now, track_in_independent_mode: true
     uncompleted_solution = create :solution, user: user, exercise: create(:exercise, track: track), independent_mode: true, completed_at: nil, track_in_independent_mode: true
+    create :iteration, solution: completed_solution
+    create :iteration, solution: uncompleted_solution
     user_track = create :user_track, user: user, track: track
 
     SwitchTrackToMentoredMode.(user, track)
@@ -19,5 +21,45 @@ class SwitchTrackToMentoredModeTest < ActiveSupport::TestCase
     refute completed_solution.track_in_independent_mode
     refute uncompleted_solution.track_in_independent_mode
   end
-end
 
+  test "correct exercises get deleted and unlocked" do
+    git_sha = SecureRandom.uuid
+    Git::ExercismRepo.stubs(current_head: git_sha)
+
+    user = create :user
+    track = create :track
+    c3 = create :exercise, track: track, core: true, position: 3
+    c2 = create :exercise, track: track, core: true, position: 2
+    c1 = create :exercise, track: track, core: true, position: 1
+    c1s1 = create :exercise, track: track, unlocked_by: c1
+    c1s2 = create :exercise, track: track, unlocked_by: c1
+    c2s1 = create :exercise, track: track, unlocked_by: c2
+    c3s1 = create :exercise, track: track, unlocked_by: c3
+    bonus = create :exercise, track: track
+
+    # A solution that's got a iteration
+    c3s1_sol = create :solution, exercise: c3s1, independent_mode: true, user: user
+    create :iteration, solution: c3s1_sol
+
+    # A completed core
+    c1_sol = create :solution, exercise: c1, approved_by: create(:user), completed_at: Time.now - 1.day, independent_mode: true, user: user
+    create :iteration, solution: c1_sol
+
+    # An unlocked side
+    c1s2_sol = create :solution, exercise: c1s2, independent_mode: true, user: user
+
+    # A different unlocked side that shouldn't be unlocked
+    c2s1_sol = create :solution, exercise: c2s1, independent_mode: true, user: user
+
+    user_track = create :user_track, user: user, track: track
+
+    SwitchTrackToMentoredMode.(user, track)
+
+    actual = user_track.solutions
+    actual.include?(c1_sol)
+    actual.include?(c3s1_sol)
+
+    expected_exercises = [ c1, c2, c1s1, c1s2, c3s1 ].map(&:id).sort
+    assert_equal expected_exercises, user_track.solutions.map(&:exercise_id).sort
+  end
+end
