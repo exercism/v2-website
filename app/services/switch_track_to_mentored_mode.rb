082ba03c5ad282b8f2def77485f5e2f08d6fe7d5
Jeremy Walker <jez.walker@gmail.com>
2018-07-29 12:16:04 +0200

    Don't delete solutions when switching modes

--- app/services/switch_track_to_mentored_mode.rb
+++ app/services/switch_track_to_mentored_mode.rb
@@ -11,15 +11,7 @@ class SwitchTrackToMentoredMode
     user_track.update(independent_mode: false)
     user_track.solutions.update_all(track_in_independent_mode: false)
 
-    # Delete all unsubmitted exercises
-    user_track.solutions.not_started.destroy_all
-
-    # Get the core exercises and unlock their dependants
-    exercise_ids = Exercise.core.where(id: user_track.solutions.completed.map(&:exercise_id)).pluck(:id)
-    Exercise.where(unlocked_by: exercise_ids).each { |e|CreateSolution.(user, e) }
-
-    # Make sure there is one unlocked core
-    UnlocksNextCoreExercise.(track, user)
+    FixUnlockingInUserTrack.(user_track)
 
     # Set everything that's not started to mentored mode
     user_track.solutions.update_all(independent_mode: true)
---
+++ test/services/fix_unlocking_in_user_track_test.rb
@@ -0,0 +1,56 @@
+require "test_helper"
+
+class FixUnlockingInUserTrackTest < ActiveSupport::TestCase
+
+  test "correct exercises get deleted and unlocked" do
+    git_sha = SecureRandom.uuid
+    Git::ExercismRepo.stubs(current_head: git_sha)
+
+    user = create :user
+    track = create :track
+    c3 = create :exercise, track: track, core: true, position: 3
+    c2 = create :exercise, track: track, core: true, position: 2
+    c1 = create :exercise, track: track, core: true, position: 1
+    c1s1 = create :exercise, track: track, unlocked_by: c1
+    c1s2 = create :exercise, track: track, unlocked_by: c1
+    c2s1 = create :exercise, track: track, unlocked_by: c2
+    c3s1 = create :exercise, track: track, unlocked_by: c3
+    bonus1 = create :exercise, track: track
+    bonus2 = create :exercise, track: track
+
+    # A solution that's got a iteration
+    # This should be persisted
+    c3s1_sol = create :solution, exercise: c3s1, independent_mode: true, user: user
+    create :iteration, solution: c3s1_sol
+
+    # A completed core
+    # This should be persisted
+    c1_sol = create :solution, exercise: c1, approved_by: create(:user), completed_at: Time.now - 1.day, independent_mode: true, user: user
+    create :iteration, solution: c1_sol
+
+    # An unlocked side
+    # This should be persisted
+    c1s2_sol = create :solution, exercise: c1s2, independent_mode: true, user: user
+
+    # A different unlocked side that shouldn't be unlocked
+    # This should not be persisted as it's not been started and it shouldn't be unlocked
+    c2s1_sol = create :solution, exercise: c2s1, independent_mode: true, user: user
+
+    # A bonus exercise. This one should be maintained
+    # but one shouldn't be created for bonus2, as that can be
+    # unlocked on demand within the UI.
+    bonus1_sol = create :solution, exercise: bonus1, user: user
+
+    user_track = create :user_track, user: user, track: track
+    FixUnlockingInUserTrack.(user_track)
+
+    actual = user_track.solutions
+    assert actual.include?(c1_sol)
+    assert actual.include?(c1s2_sol)
+    assert actual.include?(c3s1_sol)
+    assert actual.include?(bonus1_sol)
+
+    expected_exercises = [ c1, c2, c1s1, c1s2, c3s1, bonus1 ].map(&:id).sort
+    assert_equal expected_exercises, user_track.solutions.map(&:exercise_id).sort
+  end
+end
