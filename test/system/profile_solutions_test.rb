b8b97cedca344d9bd02f674829baefc023a1687e
Patrick McSweeny <patmcsweeny@gmail.com>
2018-08-01 11:44:43 -0400

    Add published_at to order clause in profile solutions query

--- test/system/profile_solutions_test.rb
+++ test/system/profile_solutions_test.rb
@@ -1,7 +1,7 @@
 require 'application_system_test_case'
 
 class ProfileSolutionsTest < ApplicationSystemTestCase
-  test "orders solutions by number of reactions" do
+  test "orders solutions by number of reactions, then by date published" do
     user = create(:user,
                   accepted_terms_at: Date.new(2016, 12, 25),
                   accepted_privacy_policy_at: Date.new(2016, 12, 25))
@@ -12,15 +12,21 @@ class ProfileSolutionsTest < ApplicationSystemTestCase
     solution1 = create(:solution,
                        user: user,
                        num_reactions: 1,
-                       exercise: create(:exercise, title: "Exercise 3", track: track),
+                       exercise: create(:exercise, title: "Exercise 4", track: track),
                        published_at: Date.new(2016, 12, 25))
     create_list(:reaction, 1, solution: solution1)
     solution2 = create(:solution,
                        user: user,
-                       num_reactions: 2,
-                       exercise: create(:exercise, title: "Exercise 2", track: track),
-                       published_at: Date.new(2016, 12, 25))
+                       num_reactions: 1,
+                       exercise: create(:exercise, title: "Exercise 3", track: track),
+                       published_at: Date.new(2016, 12, 26))
     create_list(:reaction, 2, solution: solution2)
+    solution3 = create(:solution,
+                        user: user,
+                        num_reactions: 2,
+                        exercise: create(:exercise, title: "Exercise 2", track: track),
+                        published_at: Date.new(2016, 12, 25))
+    create_list(:reaction, 3, solution: solution3)
     solution3 = create(:solution,
                         user: user,
                         num_reactions: 3,
@@ -32,7 +38,7 @@ class ProfileSolutionsTest < ApplicationSystemTestCase
     visit profile_path(user.handle)
 
     exercises = page.find_all(".solution .title").map(&:text)
-    assert_equal ["Exercise 1", "Exercise 2", "Exercise 3"], exercises
+    assert_equal ["Exercise 1", "Exercise 2", "Exercise 3", "Exercise 4"], exercises
   end
 
   test "updates solutions count correctly" do
