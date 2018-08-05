9b6b9a18eaec1fbdcc4269092e0b5f7b00542697
Jeremy Walker <jez.walker@gmail.com>
2018-08-05 22:59:19 +0100

    Show comments on auto approve

--- test/system/my/solution_discussion_section_test.rb
+++ test/system/my/solution_discussion_section_test.rb
@@ -15,7 +15,48 @@ class My::SolutionDiscussionSectionTest < ApplicationSystemTestCase
     visit my_solution_path(solution)
 
     assert_selector ".discussion h3", text: "Mentor Discussion"
+    assert_selector ".discussion form"
   end
+
+  test "m/m section with auto approve" do
+    Git::ExercismRepo.stubs(current_head: "dummy-sha1")
+
+    user = create(:user,
+                  accepted_terms_at: Date.new(2016, 12, 25),
+                  accepted_privacy_policy_at: Date.new(2016, 12, 25))
+    exercise = create :exercise, auto_approve: true
+    solution = create(:solution, user: user, track_in_independent_mode: false, independent_mode: false, exercise: exercise)
+    create :iteration, solution: solution
+    create(:user_track, track: solution.track, user: user)
+
+    sign_in!(user)
+    visit my_solution_path(solution)
+
+    assert_selector ".discussion h3", text: "Mentor Discussion"
+    refute_selector ".discussion .posts"
+    refute_selector ".discussion form"
+  end
+
+  test "m/m section with auto approve and comment" do
+    Git::ExercismRepo.stubs(current_head: "dummy-sha1")
+
+    user = create(:user,
+                  accepted_terms_at: Date.new(2016, 12, 25),
+                  accepted_privacy_policy_at: Date.new(2016, 12, 25))
+    exercise = create :exercise, auto_approve: true
+    solution = create(:solution, user: user, track_in_independent_mode: false, independent_mode: false, exercise: exercise)
+    iteration = create :iteration, solution: solution
+    create :discussion_post, iteration: iteration
+    create(:user_track, track: solution.track, user: user)
+
+    sign_in!(user)
+    visit my_solution_path(solution)
+
+    assert_selector ".discussion h3", text: "Mentor Discussion"
+    assert_selector ".discussion .posts"
+    refute_selector ".discussion form"
+  end
+
 
   test "independent mode / mentored solution" do
     Git::ExercismRepo.stubs(current_head: "dummy-sha1")
@@ -31,6 +72,7 @@ class My::SolutionDiscussionSectionTest < ApplicationSystemTestCase
     visit my_solution_path(solution)
 
     assert_selector ".discussion h3", text: "Mentor Discussion"
+    assert_selector ".discussion form"
   end
 
   test "mentored mode / independent solution" do
