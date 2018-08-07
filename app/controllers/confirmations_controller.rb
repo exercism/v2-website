ee0ab7dccf7ef6e07772c9b50ae5e29b4a1cb74f
Jack Hughes <jack@jackhughesweb.com>
2018-08-07 12:23:01 +0100

    Fixed broken resending of confirmation emails

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,14 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   layout :layout_from_site_context
 
-  def create
-  end
-
   def required
   end
 
-  private
-
-  def after_resending_confirmation_instructions_path_for
-  end
 end
