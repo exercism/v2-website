f2429e10b5768f02800753fbc90bbe30cca58173
Jeremy Walker <jez.walker@gmail.com>
2018-08-09 10:54:11 +0200

    Remove use of const in JS which doesn't compile upon deploy

--- app/assets/javascripts/my-track-mentored.js
+++ app/assets/javascripts/my-track-mentored.js
@@ -1,9 +1,9 @@
 window.setupMyTrackMentored = function() {
   $(".core-exercises .unlocked-exercises a.unlocked-exercise").each(function(idx, el){
-    const $el = $(el)
-    const title = $el.data("title")
-    const status = $el.data("status")
-    const section = $el.parents(".unlocked-exercises").siblings("h3")
+    var $el = $(el)
+    var title = $el.data("title")
+    var status = $el.data("status")
+    var section = $el.parents(".unlocked-exercises").siblings("h3")
     $el.hover(
       function(){
         section.html("<strong>" + title + "</strong>: " + status)
