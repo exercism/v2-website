0b34f3d863a6a075b97df920e64950d4a66a72fa
Jeremy Walker <jez.walker@gmail.com>
2018-08-13 19:09:07 +0100

    Fix intermitently failing test

--- test/services/switch_solution_to_mentored_mode_test.rb
+++ test/services/switch_solution_to_mentored_mode_test.rb
@@ -2,26 +2,28 @@ require 'test_helper'
 
 class SwitchSolutionToMentoredModeTest < ActiveSupport::TestCase
   test "sets uncompleted solutions to independent mode" do
-    solution = create :solution,
-                      independent_mode: true,
-                      completed_at: Time.now - 1.week,
-                      published_at: Time.now - 1.week,
-                      approved_by: create(:user),
-                      last_updated_by_user_at: Time.now - 1.week,
-                      updated_at: Time.now - 1.week
+    Timecop.freeze do
+      solution = create :solution,
+                        independent_mode: true,
+                        completed_at: Time.now - 1.week,
+                        published_at: Time.now - 1.week,
+                        approved_by: create(:user),
+                        last_updated_by_user_at: Time.now - 1.week,
+                        updated_at: Time.now - 1.week
 
-    ut = create :user_track, user: solution.user, track: solution.track
-    UserTrack.any_instance.stubs(mentoring_allowance_used_up?: false)
+      ut = create :user_track, user: solution.user, track: solution.track
+      UserTrack.any_instance.stubs(mentoring_allowance_used_up?: false)
 
-    SwitchSolutionToMentoredMode.(solution)
+      SwitchSolutionToMentoredMode.(solution)
 
-    solution.reload
-    refute solution.independent_mode?
-    assert_nil solution.completed_at
-    assert_nil solution.published_at
-    assert_nil solution.approved_by
-    assert_equal Time.now.to_i, solution.last_updated_by_user_at.to_i
-    assert_equal Time.now.to_i, solution.updated_at.to_i
+      solution.reload
+      refute solution.independent_mode?
+      assert_nil solution.completed_at
+      assert_nil solution.published_at
+      assert_nil solution.approved_by
+      assert_equal Time.now.to_i, solution.last_updated_by_user_at.to_i
+      assert_equal Time.now.to_i, solution.updated_at.to_i
+    end
   end
 
   test "doesn't change if mentoring allowance used up" do
