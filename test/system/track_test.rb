810a2516285309bbfe238b20f74340d880afb121
Jack Hughes <jack@jackhughesweb.com>
2018-08-17 16:09:57 +0100

    Added tests for exercise counts

--- test/system/track_test.rb
+++ test/system/track_test.rb
@@ -17,4 +17,30 @@ class TrackTest < ApplicationSystemTestCase
     visit track_path(track)
     within(".widget-maintainer") { assert_text "Ryan Potts" }
   end
+
+  test "shows correct exercise count on track page" do
+    Track.any_instance.stubs(:about)
+    track = create(:track)
+    exercise = create(:exercise, track: track, core: true)
+    create(:exercise,
+           track: track,
+           title: "Core Exercise",
+           core: true,
+           unlocked_by: exercise)
+    create(:exercise,
+           track: track,
+           core: false,
+           unlocked_by: exercise)
+    create(:exercise,
+           track: track,
+           core: false,
+           unlocked_by: exercise,
+           active: false)
+
+    visit track_path(track)
+    within(".overview-section") { assert_text /3 [Ee]xercises/ }
+    within(".overview-section") { refute_text /4 [Ee]xercises/ }
+    within(".exercises-section p") { assert_text /3 [Ee]xercises/ }
+    within(".exercises-section p") { refute_text /4 [Ee]xercises/ }
+  end
 end
