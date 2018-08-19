c60d64b1f4b709236d5c0a8f07b4725eef63897e
Jeremy Walker <jez.walker@gmail.com>
2018-08-19 16:29:49 +0100

    Add failing tests for the various changes

--- test/services/flux/merge_js_and_ecma_test.rb
+++ test/services/flux/merge_js_and_ecma_test.rb
@@ -7,9 +7,9 @@ module Flux
       @ecma = create :track, slug: "ecmascript"
       @ruby = create :track, slug: "ruby"
 
-      @js_hamming = create :exercise, slug: 'hamming', track: @js
-      @ecma_hamming = create :exercise, slug: 'hamming', track: @ecma
-      @ruby_hamming = create :exercise, slug: 'hamming', track: @ruby
+      @js_hamming = create :exercise, slug: 'hamming', title: "Hamming", track: @js
+      @ecma_hamming = create :exercise, slug: 'hamming', title: "Hamming", track: @ecma
+      @ruby_hamming = create :exercise, slug: 'hamming', title: "Hamming", track: @ruby
       @js_bob = create :exercise, slug: 'bob', track: @js
       @ecma_bob = create :exercise, slug: 'bob', track: @ecma
       @ruby_bob = create :exercise, slug: 'bob', track: @ruby
@@ -19,39 +19,23 @@ module Flux
       assert_equal "6a8a5a41b89a45008b46ca18ff7ea800baca1c4c", MergeJSAndECMA::JS_SHA
     end
 
-    test "migrates the solutions correctly" do
-      Timecop.freeze do
+    test "FixUnlockingInUserTrack is called for affected users" do
+      ut1 = create :user_track, user: js_user, track: @js
+      ut2 = create :user_track, user: ecma_user, track: @ecma
+      ut3 = create :user_track, user: both_user, track: @ecma
+      ut4 = create :user_track, user: both_user, track: @js
+      ut5 = create :user_track, user: ruby_user, track: @ruby
 
-        js_bob_solution = create :solution, exercise: @js_bob
-        ecma_bob_solution = create :solution, exercise: @ecma_bob
-        ruby_bob_solution = create :solution, exercise: @ruby_bob
+      FixUnlockingInUserTrack.expects(:call).with(ut1)
+      FixUnlockingInUserTrack.expects(:call).with(ut2)
+      FixUnlockingInUserTrack.expects(:call).with(ut3)
 
-        js_hamming_solution = create :solution, exercise: @js_hamming
-        ecma_hamming_solution = create :solution, exercise: @ecma_hamming
-        ruby_hamming_solution = create :solution, exercise: @ruby_hamming
-
-        MergeJSAndECMA.()
-
-        [
-          js_bob_solution, ecma_bob_solution, ruby_bob_solution,
-          js_hamming_solution, ecma_hamming_solution, ruby_hamming_solution
-        ].each(&:reload)
-
-        # Check JS has changed
-        assert_equal @ecma_bob, js_bob_solution.exercise
-        assert_equal @ecma_hamming, js_hamming_solution.exercise
-
-        # Check ecma is unchanged
-        assert_equal @ecma_bob, ecma_bob_solution.exercise
-        assert_equal @ecma_hamming, ecma_hamming_solution.exercise
-
-        # Check Ruby is unchanged
-        assert_equal @ruby_bob, ruby_bob_solution.exercise
-        assert_equal @ruby_hamming, ruby_hamming_solution.exercise
-      end
+      # This shouldn't be called for the two js tracks
+      FixUnlockingInUserTrack.expects(:call).with(ut4).never
+      FixUnlockingInUserTrack.expects(:call).with(ut5).never
     end
 
-    test "locks the JS Sha" do
+    test "locks the JS solution shas" do
       ecma_sha = SecureRandom.uuid
 
       js_bob_solution = create :solution, exercise: @js_bob, git_sha: SecureRandom.uuid
@@ -63,6 +47,216 @@ module Flux
 
       assert_equal MergeJSAndECMA::JS_SHA, js_bob_solution.git_sha
       assert_equal ecma_sha, ecma_bob_solution.git_sha
+    end
+
+    test "create ecma user track and removes js user tracks" do
+      ecma_user = create :user
+      mentored_js_user = create :user
+      independent_js_user = create :user
+      both_user = create :user
+      ruby_user = create :user
+
+      ecma_user_track = create :user_track, user: ecma_user, track: @ecma
+      mentored_js_track = create :user_track, user: mentored_js_user, track: @js, independent_mode: true
+      independent_js_track = create :user_track, user: independent_js_user, track: @js, independent_mode: false
+      both_js_track = create :user_track, user: both_user, track: @js
+      both_ecma_track = create :user_track, user: both_user, track: @ecma
+      ruby_user_track = create :user_track, user: ruby_user, track: @ruby
+
+      MergeJSAndECMA.()
+
+      # Check we've just repointed the existing tracks
+      [mentored_js_track, independent_js_track].reload
+      assert_equal @ecma, mentored_js_track.track
+      assert_equal @ecma, independent_js_track.track
+
+      # Check there is an ecma track for everyone and a js track for no-one
+      assert_equal [mentored_js_user, independent_js_user, ecma_user, both_user].map(&:id), UserTrack.where(track: @ecma).pluck(:user_id)
+      assert_equal [].map(&:id), UserTrack.where(track: @js).pluck(:user_id)
+
+      # Check the actual ecma user track is still the same object
+      assert_equal ecma_user_track, UserTrack.find_by(track: @ecma, user: ecma_user)
+
+      # Check the ruby track is unaffected
+      assert_equal ruby_user_track, UserTrack.find_by(track: @ruby, user: ruby_user)
+
+      # Check indepedent_mode is honoured
+      assert_equal UserTrack.find_by(track: @js, user: mentored_js_user).independent_mode
+      refute UserTrack.find_by(track: @js, user: independent_js_user).independent_mode
+    end
+
+    test "create ecma track_mentorship" do
+      ecma_mentor = create :user
+      js_mentor = create :user
+      both_mentor = create :user
+      ruby_mentor = create :user
+
+      ecma_track_mentorship = create :track_mentorship, user: ecma_mentor, track: @ecma
+      create :track_mentorship, user: js_mentor, track: @js
+      create :track_mentorship, user: both_mentor, track: @js
+      create :track_mentorship, user: both_mentor, track: @ecma
+      ruby_track_mentorship = create :track_mentorship, user: ruby_mentor, track: @ruby
+
+      MergeJSAndECMA.()
+
+      # Check there is an ecma track mentorship for everyone and a js track mentorship for no-one
+      assert_equal [js_mentor, ecma_mentor, both_mentor].map(&:id), TrackMentorship.where(track: @ecma).pluck(:user_id)
+      assert_equal [].map(&:id), TrackMentorship.where(track: @js).pluck(:user_id)
+
+      # Check the actual ecma track mentorship is still the same object
+      assert_equal ecma_track_mentorship, TrackMentorship.find_by(track: @ecma, user: ecma_mentor)
+
+      # Check the ruby track mentorship is unaffected
+      assert_equal ruby_track_mentorship, TrackMentorship.find_by(track: @ruby, user: ruby_mentor)
+    end
+
+    test "renames the exercises correctly" do
+      MergeJSAndECMA.()
+
+      [@js_hamming, @ecma_hamming, @ruby_hamming].each(&:reload)
+
+      assert_equal 'legacy-hamming', @js_hamming.slug
+      assert_equal 'Hamming (Legacy)', @js_hamming.title
+
+      assert_equal 'hamming', @ecma_hamming.slug
+      assert_equal 'Hamming', @ecma_hamming.title
+
+      assert_equal 'hamming', @ruby_hamming.slug
+      assert_equal 'Hamming', @ruby_hamming.title
+    end
+
+    test "ruby and ecma exercises don't get touched" do
+      Timecop.freeze do
+
+        ecma_bob_solution = create_solution(@ecma_bob, :started)
+        ruby_bob_solution = create_solution(@ruby_bob, :started)
+        ecma_hamming_solution = create_solution(@ecma_hamming, :started)
+        ruby_hamming_solution = create_solution(@ruby_hamming, :started)
+
+        MergeJSAndECMA.()
+
+        [
+          ecma_bob_solution, ruby_bob_solution, ecma_hamming_solution, ruby_hamming_solution
+        ].each(&:reload)
+
+        # Check ecma is unchanged
+        assert_equal @ecma_bob, ecma_bob_solution.exercise
+        assert_equal @ecma_hamming, ecma_hamming_solution.exercise
+
+        # Check Ruby is unchanged
+        assert_equal @ruby_bob, ruby_bob_solution.exercise
+        assert_equal @ruby_hamming, ruby_hamming_solution.exercise
+      end
+    end
+
+    test "exercises are migrated if there is no clash" do
+      js_bob_solution = create_solution(@js_bob, :started)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @ecma_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and ecma is approved" do
+      js_bob_solution = create_solution(@js_bob, :started)
+      ecma_bob_solution = create_solution(@ecma_bob, :approved)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and ecma is completed" do
+      js_bob_solution = create_solution(@js_bob, :started)
+      ecma_bob_solution = create_solution(@ecma_bob, :completed)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to ecma exercise if there is a clash and js is completed but ecma is started" do
+      js_bob_solution = create_solution(@js_bob, :completed)
+      ecma_bob_solution = create_solution(@ecma_bob, :started)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @ecma_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to ecma exercise if there is a clash and js is approved but ecma is started" do
+      js_bob_solution = create_solution(@js_bob, :approved)
+      ecma_bob_solution = create_solution(@ecma_bob, :started)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @ecma_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and js is approved and ecma is completed" do
+      js_bob_solution = create_solution(@js_bob, :approved)
+      ecma_bob_solution = create_solution(@ecma_bob, :completed)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and js is completed and ecma is approved" do
+      js_bob_solution = create_solution(@js_bob, :completed)
+      ecma_bob_solution = create_solution(@ecma_bob, :approved)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and both are approved" do
+      js_bob_solution = create_solution(@js_bob, :approved)
+      ecma_bob_solution = create_solution(@ecma_bob, :approved)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and both are completed" do
+      js_bob_solution = create_solution(@js_bob, :completed)
+      ecma_bob_solution = create_solution(@ecma_bob, :completed)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    test "exercises are migrated to legacy exercise if there is a clash and both are started" do
+      js_bob_solution = create_solution(@js_bob, :started)
+      ecma_bob_solution = create_solution(@ecma_bob, :started)
+
+      MergeJSAndECMA.()
+      js_bob_solution.reload
+
+      assert_equal @js_bob, js_bob_solution.exercise
+    end
+
+    def create_solution(exercise, state, user = create(:user))
+      case state
+      when :started
+        create :solution, exercise: exercise, downloaded_at: Time.now, user: user
+      when :approved
+        create :solution, exercise: exercise, approved_by: create(:user), user: user
+      when :completed
+        create :solution, exercise: exercise, completed_at: Time.now, user: user
+      end
     end
   end
 end
