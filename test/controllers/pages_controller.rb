bcc7d95e96d9cb950d04267cebaedb3685d97268
Jeremy Walker <jez.walker@gmail.com>
2018-08-20 15:49:31 +0100

    Add test coverage to pages

---
+++ test/controllers/pages_controller.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "index page" do
+    get "/"
+    assert_response :success
+  end
+
+  test "cli_walkthrough page" do
+    get cli_walkthrough_page_path
+    assert_response :success
+  end
+
+  PagesController::PAGES.each do |(title, page)|
+    test "page works signed in: #{page}" do
+      sign_in!
+      get send("#{page}_page_path")
+      assert_response :success
+    end
+  end
+
+  PagesController::PAGES.each do |(title, page)|
+    test "page works signed out: #{page}" do
+      get send("#{page}_page_path")
+      assert_response :success
+    end
+  end
+
+  PagesController::LICENCES.each do |(title, licence)|
+    test "licence works signed in: #{licence}" do
+      sign_in!
+      get send("#{licence}_licence_path")
+      assert_response :success
+    end
+  end
+
+  PagesController::LICENCES.each do |(title, licence)|
+    test "licence works signed out: #{licence}" do
+      get send("#{licence}_licence_path")
+      assert_response :success
+    end
+  end
+end
