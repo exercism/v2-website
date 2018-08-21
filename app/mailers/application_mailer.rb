90abe317055965d39465dc77dbc20a68779cdbf4
Karlo Soriano <karlo@thalamus.ai>
2018-08-21 11:44:49 +0800

    Raise clearer error when SMTP server goes down

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "The Exercism Team <hello@exercism.io>"
   layout 'mailer'
+
+  class UnableToConnectToSMTPServerError < StandardError
+  end
+
+  rescue_from EOFError do |exception|
+    raise UnableToConnectToSMTPServerError, "Unable to connect to SMTP server. Please check https://twitter.com/sparkpostops"
+  end
 end
