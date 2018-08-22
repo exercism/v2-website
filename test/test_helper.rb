40515d13b4a8ae177aea8813eae86ca9229eae81
Jeremy Walker <jez.walker@gmail.com>
2018-08-22 12:24:25 +0100

    Add codeclimate support to travis file (#295)
    * Add codeclimate support to travis file
    
    * Install simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+require "simplecov"
+
+SimpleCov.start "rails"
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/minitest'
