0c148b0fdd940aca59c2df70107595a8b10c0529
Jack Hughes <jack@jackhughesweb.com>
2018-08-24 15:39:11 +0100

    Changed notifications helper test to use discussion post trigger instead of solution

--- test/helpers/notifications_helper_test.rb
+++ test/helpers/notifications_helper_test.rb
@@ -5,8 +5,10 @@ class NotificationsHelperTest < ActionView::TestCase
     user = create :user
 
     solution = create :solution, user: user
-    notification1 = create :notification, type: 'new_discussion_post', trigger: solution
-    notification2 = create :notification, type: 'new_discussion_post_for_mentor', trigger: solution
+    iteration = create :iteration, solution: solution
+    discussion_post = create :discussion_post, user: user, iteration: iteration
+    notification1 = create :notification, type: 'new_discussion_post', trigger: discussion_post
+    notification2 = create :notification, type: 'new_discussion_post_for_mentor', trigger: discussion_post
     notification3 = create :notification, type: 'new_iteration_for_mentor', about: solution
 
     assert_dom_equal user.avatar_url, notification_image(notification1)
