7f78d08aa943d821abe4991f0befcf20635c1db7
Sean Fairchild <Sean.Fairchild@bhnetwork.com>
2018-08-31 12:46:38 -0400

    Refactor code_snippet

--- app/assets/javascripts/code_snippet.js
+++ app/assets/javascripts/code_snippet.js
@@ -1,17 +1,17 @@
 $('.widget-code-snippet button.copy-button').click(function() {
+  const updateButton = function (that, message) {
+    const oldText = $(that).text();
+    $(that).text(message)
+    resetButton(that, oldText);
+  }
+
+  const resetButton = function (that, newMessage) {
+    setTimeout(function() {
+      $(that).text(newMessage)
+    }, 2000);
+  }
+
   $(this).parents('.widget-code-snippet').find('input.download-code').select();
   document.execCommand('copy');
   updateButton(this, 'copied!');
 });
-
-const updateButton = function (that, message) {
-  const oldText = $(that).text();
-  $(that).text(message)
-  resetButton(that, oldText);
-}
-
-const resetButton = function (that, newMessage) {
-  setTimeout(function() {
-    $(that).text(newMessage)
-  }, 2000);
-}
