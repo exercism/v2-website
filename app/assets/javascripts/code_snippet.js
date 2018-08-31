bf7253646a7a8936dd542c27e702b9dc23317d27
Sean Fairchild <Sean.Fairchild@bhnetwork.com>
2018-08-30 23:26:35 -0400

    Alert user when text copied

--- app/assets/javascripts/code_snippet.js
+++ app/assets/javascripts/code_snippet.js
@@ -1,4 +1,17 @@
 $('.widget-code-snippet button.copy-button').click(function() {
   $(this).parents('.widget-code-snippet').find('input.download-code').select();
   document.execCommand('copy');
-})
+  updateButton(this, 'copied!');
+});
+
+const updateButton = function (that, message) {
+  const oldText = $(that).text();
+  $(that).text(message)
+  resetButton(that, oldText);
+}
+
+const resetButton = function (that, newMessage) {
+  setTimeout(function() {
+    $(that).text(newMessage)
+  }, 2000);
+}
