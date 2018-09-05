163ed5a4760088567496aa6308be5b47c2375541
Jack Hughes <jack@jackhughesweb.com>
2018-09-05 21:36:50 +0100

    Added dark theme to user model, with toggle in preferences

--- app/controllers/my/preferences_controller.rb
+++ app/controllers/my/preferences_controller.rb
@@ -3,11 +3,18 @@ class My::PreferencesController < MyController
   end
 
   def update
-    current_user.communication_preferences.update(
-      params.require(:communication_preferences).reject {|k,v|
-        ['user_id', 'created_at', 'updated_at'].include?(k)
-      }.permit!
-    )
-    redirect_to({action: :edit}, notice: "Communication preferences updated successfully")
+    if params[:communication_preferences].present?
+      current_user.communication_preferences.update(
+        params.require(:communication_preferences).reject {|k,v|
+          ['user_id', 'created_at', 'updated_at'].include?(k)
+        }.permit!
+      )
+      redirect_to({action: :edit}, notice: "Communication preferences updated successfully")
+    elsif params[:update_dark_theme].present?
+      current_user.update(dark_code_theme: (params[:dark_code_theme] == "1"))
+      redirect_to({action: :edit}, notice: "Code theme preference updated successfully")
+    else
+      redirect_to({action: :edit})
+    end
   end
 end
