f06d927a20033d2678a54cc961ae15feb1a052ce
Jack Hughes <jack@jackhughesweb.com>
2018-09-05 21:03:45 +0100

    Renamed 'communication_preferences' to 'preferences'

--- test/system/my/preferences_test.rb
+++ test/system/my/preferences_test.rb
@@ -1,6 +1,6 @@
 require 'application_system_test_case'
 
-class My::SettingsTest < ApplicationSystemTestCase
+class My::SettingsPreferencesTest < ApplicationSystemTestCase
   setup do
     Git::ExercismRepo.stubs(current_head: "dummy-sha1")
     @user = create(:user,
@@ -10,10 +10,10 @@ class My::SettingsTest < ApplicationSystemTestCase
     sign_in!(@user)
   end
 
-  test "can navigate to communication preferences" do
+  test "can navigate to preferences" do
     visit my_settings_path
-    assert page.has_link?(href: edit_my_settings_communication_preferences_path)
+    assert page.has_link?(href: edit_my_settings_preferences_path)
     click_on "Preferences"
-    assert_current_path edit_my_settings_communication_preferences_path
+    assert_current_path edit_my_settings_preferences_path
   end
 end
