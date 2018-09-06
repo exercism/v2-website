2fbce56f05876fbd4d789242c66c84b62bafb63b
Jack Hughes <jack@jackhughesweb.com>
2018-09-06 12:38:16 +0100

    Added system tests to react and unreact to a solution

--- test/system/solutions_test.rb
+++ test/system/solutions_test.rb
@@ -33,4 +33,43 @@ class SolutionsTest < ApplicationSystemTestCase
     sign_in!(user)
     visit track_exercise_solutions_path(solution.track, solution.exercise)
   end
+
+  test "can react to a solution" do
+    user = create(:user,
+                  accepted_terms_at: Date.new(2016, 12, 25),
+                  accepted_privacy_policy_at: Date.new(2016, 12, 25))
+    solution = create(:solution, published_at: Time.now)
+
+    sign_in!(user)
+    visit solution_path(solution)
+
+    assert_equal 0, solution.reactions.count
+
+    assert_selector ".react", text: "React to this solution"
+    find(".react .like").click
+    assert_selector ".react .like.selected"
+
+    solution.reload
+    assert_equal 1, solution.reactions.count
+  end
+
+  test "can unreact to a solution" do
+    user = create(:user,
+                  accepted_terms_at: Date.new(2016, 12, 25),
+                  accepted_privacy_policy_at: Date.new(2016, 12, 25))
+    solution = create(:solution, published_at: Time.now)
+    reaction = create(:reaction, user: user, solution: solution, emotion: :like)
+
+    sign_in!(user)
+    visit solution_path(solution)
+
+    assert_equal 1, solution.reactions.count
+
+    assert_selector ".react", text: "React to this solution"
+    find(".react .like.selected").click
+    assert_no_selector ".react .like.selected", wait: 10
+
+    solution.reload
+    assert_equal 0, solution.reactions.count
+  end
 end
