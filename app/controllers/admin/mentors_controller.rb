9387c6c86aae3dfeb3f1b026034b781524fbec9a
amaliacardenas <amaliacardenas@gmail.com>
2018-09-26 18:00:26 +0200

    Updating draft

--- app/controllers/admin/mentors_controller.rb
+++ app/controllers/admin/mentors_controller.rb
@@ -1,7 +1,15 @@
 class Admin::MentorsController < ApplicationController
+
   def index
-    @user = User.all()
-    @mentor = TrackMentorship.all()
+    @user_ids = TrackMentorship.distinct.pluck(:user_id)
+    @users = User.where(id: @user_ids).page(params[:page]).per(20)
+    @tracks = Hash.new
+    @user_ids.each do |user_id|
+      @tracks[user_id] = TrackMentorship.where(user_id: user_id).to_a().map {
+        |mentorship|
+        mentorship.track.title
+      } #.first().track.introduction
+    end
   end
 
   def show
