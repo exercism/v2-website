5b319699a44c2d37cc065d41ea67d557b0ef52be
amaliacardenas <amaliacardenas@gmail.com>
2018-09-27 15:02:46 +0200

    Add pagination of mentor list to admin section for mentors.
    Co-Authored-By: loriking <loriannking@gmail.com>

--- app/controllers/admin/mentors_controller.rb
+++ app/controllers/admin/mentors_controller.rb
@@ -1,8 +1,9 @@
 class Admin::MentorsController < ApplicationController
 
   def index
+    mentors_per_page = 30
     @user_ids = TrackMentorship.distinct.pluck(:user_id)
-    @users = User.where(id: @user_ids)
+    @users = User.where(id: @user_ids).page(params[:page]).per(mentors_per_page)
     @tracks = Hash.new
     @user_ids.each do |user_id|
       @tracks[user_id] = TrackMentorship.where(user_id: user_id).to_a().map {
