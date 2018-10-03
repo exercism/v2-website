3bec6e47730b4ad5a78151acb7b7e384c85d1a94
Jeremy Walker <jez.walker@gmail.com>
2018-10-03 12:25:18 -0500

    Double speed of fixing unlocking script

--- app/services/fix_unlocking_in_user_track.rb
+++ app/services/fix_unlocking_in_user_track.rb
@@ -19,11 +19,29 @@ class FixUnlockingInUserTrack
     keep_solution_ids = []
 
     # Get the core exercises and unlock their dependants
-    exercise_ids = Exercise.core.where(id: user_track.solutions.completed.map(&:exercise_id)).pluck(:id)
-    keep_solution_ids += Exercise.where(unlocked_by: exercise_ids).map { |e|CreateSolution.(user, e).id }
+    completed_exercise_ids = Exercise.core.where(id: user_track.solutions.completed.map(&:exercise_id)).pluck(:id)
+
+    # Get any existing solutions that are unlocked by completed core ids
+    existing_solution_ids, existing_exercise_ids = Solution.where(user_id: user.id).
+                                                            joins(:exercise).
+                                                            where("exercises.unlocked_by_id": completed_exercise_ids).
+                                                            pluck(:id, :exercise_id).
+                                                            transpose
+    # Add the existing solutions
+    keep_solution_ids += existing_solution_ids if existing_solution_ids
+
+    # Make any new ones
+    keep_solution_ids += Exercise.where(unlocked_by_id: completed_exercise_ids).
+                                  where.not(id: existing_exercise_ids.to_a).
+                                  map { |e|CreateSolution.(user, e).id }
 
     # Check all the bonus exercises are avalaible but don't unlock them.
-    keep_solution_ids += user_track.solutions.where(exercise_id: track.exercises.side.where(unlocked_by: nil)).pluck(:id)
+    keep_solution_ids += user_track.solutions.
+                                    where(exercise_id:
+                                            track.exercises.
+                                            side.where(unlocked_by_id: nil)
+                                         ).
+                                    pluck(:id)
 
     # Make sure there is one unlocked core
     next_exercise = track.exercises.core.
