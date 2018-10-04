a993a42b5b7ca36c289f1cce05840b61362fe771
Karlo Soriano <karlo@thalamus.ai>
2018-10-04 11:58:03 +0800

    Include bonus and unlocked exercises to the list of exercises to be unlocked

--- test/models/exercise_map_test.rb
+++ test/models/exercise_map_test.rb
@@ -12,11 +12,17 @@ class ExerciseMapTest < ActiveSupport::TestCase
                            core: false,
                            unlocked_by: nil,
                            track: track)
+    unlocked_exercise = create(:exercise,
+                               unlocked_by: exercise,
+                               track: track)
 
     core_exercises = ExerciseMap.new(user, track).core_exercises
 
     assert_delegated_equal exercise, core_exercises[0].exercise
-    assert_delegated_equal [side_exercise], core_exercises[0].unlocks
+    assert_delegated_equal(
+      [side_exercise, unlocked_exercise],
+      core_exercises[0].unlocks
+    )
   end
 
   private
