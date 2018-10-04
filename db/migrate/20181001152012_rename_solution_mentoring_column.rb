8ca3c169f32649c2e4d92fe93ecfacc736b34f2b
Jeremy Walker <jez.walker@gmail.com>
2018-10-04 12:46:03 -0500

    Set side exercises that have had mentoring to have mentoring_requested set to true

--- db/migrate/20181001152012_rename_solution_mentoring_column.rb
+++ db/migrate/20181001152012_rename_solution_mentoring_column.rb
@@ -9,6 +9,14 @@ class RenameSolutionMentoringColumn < ActiveRecord::Migration[5.2]
              submitted.
              update_all("mentoring_requested_at = last_updated_by_user_at")
 
+    # Set all side exercices that already have mentoring to have
+    # mentoring_requested set to true.
+    Solution.where(track_in_independent_mode: false).
+             joins(:exercise).merge(Exercise.side).
+             submitted.
+             has_a_mentor.
+             update_all("mentoring_requested_at = last_updated_by_user_at")
+
     # This is deliberately commented out in the migration as I want to verify
     # everything has worked in live before deleting this column.
     #remove_column :solutions, :independent_mode
