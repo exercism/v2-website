ba92a0e66020beab60ce55702645c66a6a709b7b
Jeremy Walker <jez.walker@gmail.com>
2018-10-04 15:06:22 -0500

    Improve styling of copied button

--- app/assets/javascripts/code_snippet.js
+++ app/assets/javascripts/code_snippet.js
@@ -1,14 +1,14 @@
 $('.widget-code-snippet button.copy-button').click(function() {
-  const updateButton = function (that, message) {
-    const oldText = $(that).text();
-    $(that).text(message)
-    resetButton(that, oldText);
+  const updateButton = function (btn, message) {
+    const oldText = $(btn).text();
+    $(btn).text(message).addClass('copied')
+    setTimeout(function() {
+      resetButton(btn, oldText);
+    }, 2000);
   }
 
-  const resetButton = function (that, newMessage) {
-    setTimeout(function() {
-      $(that).text(newMessage)
-    }, 2000);
+  const resetButton = function (btn, newMessage) {
+    $(btn).text(newMessage).removeClass('copied')
   }
 
   $(this).parents('.widget-code-snippet').find('input.download-code').select();
