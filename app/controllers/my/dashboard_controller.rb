8ee5d9967dfcc2c8cd99ac0dc24a5e7d91231d3f
Sean <seanf215@gmail.com>
2018-10-05 13:55:01 -0400

    Add more expressive page titles (#417)
    * Update page title metedata
    
    * Update title text wording

--- app/controllers/my/dashboard_controller.rb
+++ app/controllers/my/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class My::DashboardController < MyController
   def show
     return redirect_to [:my, :tracks]
-    @last_solution = current_user.iterations.order('id desc').first.try(:solution)
   end
 end
