4a08f005917f92b72a4fd6489446458e7a1db0ee
Patrick McSweeny <patmcsweeny@gmail.com>
2018-10-06 00:47:17 -0400

    Add test for signing out

---
+++ test/system/signout_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+
+class SignoutTest < ApplicationSystemTestCase
+  test "user signs out" do
+    user = create :user
+    sign_in! user
+    visit my_tracks_path
+    find('.misc-menu').hover
+    click_on 'Sign out'
+
+    assert_text 'Log in'
+  end
+end
