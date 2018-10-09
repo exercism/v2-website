d1c877fe26fceca993980a82357e4b16524848fc
Jeremy Walker <jez.walker@gmail.com>
2018-10-09 13:55:46 -0500

    Add landing and registration pages for becoming a mentor

--- app/controllers/mentor/registrations_controller.rb
+++ app/controllers/mentor/registrations_controller.rb
@@ -3,12 +3,13 @@ class Mentor::RegistrationsController < ApplicationController
   before_action :restrict_to_non_mentors!
 
   def new
-
+    @tracks = Track.active.order(:title).map{|t|[t.title, t.id]}
   end
 
   def create
     current_user.update(is_mentor: true)
-    redirect_to mentor_configure_path
+    current_user.track_mentorships.create(track_id: params[:track_id])
+    redirect_to mentor_dashboard_path
   end
 
   private
--- test/system/mentor/registration_test.rb
+++ test/system/mentor/registration_test.rb
@@ -4,18 +4,25 @@ class MentorRegistrationTest < ApplicationSystemTestCase
   test "mentor chooses tracks" do
     user = create :user
     ruby = create(:track, title: "Ruby")
+    python = create(:track, title: "Python")
 
     sign_in!(user)
 
     visit become_a_mentor_path
-    within("#mentor-registration-page") { click_on "Become a mentor" }
+    within("#become-a-mentor-page") {
+      click_on "Become a mentor"
+    }
 
-    check "Ruby"
-    click_on "Save settings"
+    within("#mentor-registration-page") {
+      check "accept_code_of_conduct"
+      select_option "Ruby", selector: "#track_id"
+      click_on "Become a mentor"
+    }
 
     assert_selector "#mentor-dashboard-page"
 
     user.reload
     assert user.is_mentor?
+    assert_equal [ruby], user.mentored_tracks
   end
 end
