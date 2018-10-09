b57d6d52550aa2b8489b00a91b5dfff41d531653
Jeremy Walker <jez.walker@gmail.com>
2018-10-09 14:25:09 -0500

    Improve clarity of variable name

--- app/services/fix_unlocking_in_user_track.rb
+++ app/services/fix_unlocking_in_user_track.rb
@@ -19,19 +19,19 @@ class FixUnlockingInUserTrack
     keep_solution_ids = []
 
     # Get the core exercises and unlock their dependants
-    completed_exercise_ids = Exercise.core.where(id: user_track.solutions.completed.map(&:exercise_id)).pluck(:id)
+    completed_core_exercise_ids = Exercise.core.where(id: user_track.solutions.completed.map(&:exercise_id)).pluck(:id)
 
     # Get any existing solutions that are unlocked by completed core ids
     existing_solution_ids, existing_exercise_ids = Solution.where(user_id: user.id).
                                                             joins(:exercise).
-                                                            where("exercises.unlocked_by_id": completed_exercise_ids).
+                                                            where("exercises.unlocked_by_id": completed_core_exercise_ids).
                                                             pluck(:id, :exercise_id).
                                                             transpose
     # Add the existing solutions
     keep_solution_ids += existing_solution_ids if existing_solution_ids
 
     # Make any new ones
-    keep_solution_ids += Exercise.where(unlocked_by_id: completed_exercise_ids).
+    keep_solution_ids += Exercise.where(unlocked_by_id: completed_core_exercise_ids).
                                   where.not(id: existing_exercise_ids.to_a).
                                   map { |e|CreateSolution.(user, e).id }
 
