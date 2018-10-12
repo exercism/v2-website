194897acacd87b330f3e6ae88670c4f3d4320fc6
Jeremy Walker <jez.walker@gmail.com>
2018-10-12 14:00:03 -0500

    Comment out migration

--- db/migrate/20181003205938_remove_old_mentoring_enabled_flag.rb
+++ db/migrate/20181003205938_remove_old_mentoring_enabled_flag.rb
@@ -1,5 +1,5 @@
 class RemoveOldMentoringEnabledFlag < ActiveRecord::Migration[5.2]
   def change
-    remove_column :solutions, :mentoring_enabled, :boolean, null: true
+    #remove_column :solutions, :mentoring_enabled, :boolean, null: true
   end
 end
