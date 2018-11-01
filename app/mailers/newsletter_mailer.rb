f61b52b496ce6d9b058e6efc1f84951f3a7587b0
Jeremy Walker <jez.walker@gmail.com>
2018-11-01 04:56:39 +0000

    Add mentor changes mailer

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -3,4 +3,9 @@ class NewsletterMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: "[Exercism] We've changed mentoring on side exercises")
   end
+
+  def mentor_changes_1
+    @user = params[:user]
+    mail(to: "#{@user.name} <#{@user.email}>", subject: "[Exercism] View your Mentor Rating")
+  end
 end
