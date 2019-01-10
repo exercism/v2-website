d8efb38576a0da6aba846455c014c242a90e070f
Jeremy Walker <jez.walker@gmail.com>
2019-01-10 17:43:40 +0000

    Add basic index and show for blog

---
+++ app/helpers/blog_posts_helper.rb
@@ -0,0 +1,2 @@
+module BlogPostsHelper
+end
---
+++ test/controllers/blog_posts_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class BlogPostsControllerTest < ActionDispatch::IntegrationTest
+  test "index shows published posts" do
+    published = create :blog_post, published_at: DateTime.now - 1.minute
+    scheduled = create :blog_post, published_at: DateTime.now + 1.minute
+
+    get blog_posts_path
+    assert_equal [published], assigns(:blog_posts)
+  end
+
+  test "show finds by slug" do
+    blog_post = create :blog_post, published_at: DateTime.now - 1.minute
+    get blog_post_path(id: blog_post.slug)
+    assert_equal blog_post, assigns(:blog_post)
+  end
+
+  test "show 404s for unpublished post" do
+    blog_post = create :blog_post, published_at: DateTime.now + 1.minute
+    assert_raises ActiveRecord::RecordNotFound do
+      get blog_post_path(id: blog_post.slug)
+    end
+  end
+end
