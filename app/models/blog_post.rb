9bc489c0c8566f203133d6889feade4373372f71
Jeremy Walker <jez.walker@gmail.com>
2019-01-16 10:49:13 -0800

    Avoid orphened comments

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -5,7 +5,7 @@ class BlogPost < ApplicationRecord
   end
 
   belongs_to :author, class_name: 'User', optional: true, foreign_key: 'author_handle', primary_key: 'handle'
-  has_many :comments, class_name: "BlogComment"
+  has_many :comments, class_name: "BlogComment", dependent: :destroy
 
   scope :published, -> { where('published_at <= NOW()') }
   scope :scheduled, -> { where('published_at > NOW()') }
