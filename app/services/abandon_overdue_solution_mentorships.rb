a996651399cb0d321f214e3878f7fc2bafeaa5cd
Jeremy Walker <jez.walker@gmail.com>
2019-01-17 11:31:59 -0800

    Don't abandon approved or completed solutions

--- app/services/abandon_overdue_solution_mentorships.rb
+++ app/services/abandon_overdue_solution_mentorships.rb
@@ -2,7 +2,10 @@ class AbandonOverdueSolutionMentorships
   include Mandate
 
   def call
-    SolutionMentorship.where("requires_action_since < ?", Time.current - 7.days).
+    SolutionMentorship.joins(:solution).
+                       where("requires_action_since < ?", Time.current - 7.days).
+                       where('solutions.approved_by': nil).
+                       where('solutions.completed_at': nil).
                        where(abandoned: false).
                        each do |mentorship|
       AbandonSolutionMentorship.(mentorship, :timed_out)
