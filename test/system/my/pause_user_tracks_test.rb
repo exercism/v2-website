f111c2e76cf4fedce89cd38cb3d1d5952960b510
Jeremy Walker <jez.walker@gmail.com>
2019-01-22 12:43:42 -0800

    Fix cancelling functionality of unpausing track

--- test/system/my/pause_user_tracks_test.rb
+++ test/system/my/pause_user_tracks_test.rb
@@ -38,4 +38,22 @@ class My::PauseUserTracksTest < ApplicationSystemTestCase
 
     refute user_track.reload.paused?
   end
+
+  test 'cancels unpausing the user track' do
+    user = create(:user,
+                  accepted_terms_at: Date.new(2016, 12, 25),
+                  accepted_privacy_policy_at: Date.new(2016, 12, 25))
+    track = create(:track,
+                   title: "Ruby",
+                   repo_url: "file://#{Rails.root}/test/fixtures/website-copy")
+    user_track = create :user_track, user: user, track: track, paused_at: Time.current
+
+    sign_in!(user)
+    visit my_track_path(user_track.track)
+    within("#modal") do
+      click_on "Cancel"
+    end
+    assert_selector "body.namespace-my.controller-tracks.action-index"
+    assert user_track.reload.paused?
+  end
 end
