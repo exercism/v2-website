8a765d0aa16e6aa5df00142c1b206b22615ebdc9
Jeremy Walker <jez.walker@gmail.com>
2019-01-23 21:13:04 -0800

    DRY up reactions, change icons, and improve mobile styling

---
+++ app/helpers/reactions_helper.rb
@@ -0,0 +1,14 @@
+module ReactionsHelper
+  def reaction_like_icon
+    icon 'thumbs-up fa-fw', 'Like'
+  end
+
+  def reaction_love_icon
+    icon 'heart fa-fw', 'Love'
+  end
+
+  def reaction_genius_icon
+    icon 'lightbulb-on fa-fw', 'Genius'
+  end
+end
+
