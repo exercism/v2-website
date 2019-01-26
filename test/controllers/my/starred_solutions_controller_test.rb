40980e372158cdcc0ec3a39684ee02836f969881
Jeremy Walker <jez.walker@gmail.com>
2019-01-26 11:02:49 -0800

    Tidy up failing tests

--- test/controllers/my/starred_solutions_controller_test.rb
+++ test/controllers/my/starred_solutions_controller_test.rb
@@ -4,35 +4,33 @@ class My::StarredSolutionsControllerTest < ActionDispatch::IntegrationTest
   test "create creates" do
     sign_in!
     solution = create :solution, published_at: DateTime.now
-    post my_starred_solutions_path, params: {solution_id: solution.uuid, emotion: "love", format: 'js'}
+    post my_starred_solutions_path, params: {solution_id: solution.uuid, format: 'js'}
 
     assert_response :success
     assert_equal 1, solution.stars.size
     solution_star = solution.stars.first
 
     assert_equal @current_user, solution_star.user
-    assert_equal "love", solution_star.emotion
   end
 
   test "create updates" do
     sign_in!
     solution = create :solution, published_at: DateTime.now
-    solution_star = create :solution_star, solution: solution, user: @current_user, emotion: 'like'
-    post my_starred_solutions_path, params: {solution_id: solution.uuid, emotion: "love", format: 'js'}
+    solution_star = create :solution_star, solution: solution, user: @current_user
+    post my_starred_solutions_path, params: {solution_id: solution.uuid, format: 'js'}
 
     assert_response :success
     assert_equal 1, solution.stars.size
     solution_star = solution.stars.first
 
     assert_equal @current_user, solution_star.user
-    assert_equal "love", solution_star.emotion
   end
 
   test "create destroys" do
     sign_in!
     solution = create :solution, published_at: DateTime.now
-    solution_star = create :solution_star, solution: solution, user: @current_user, emotion: 'love'
-    post my_starred_solutions_path, params: {solution_id: solution.uuid, emotion: "love", format: 'js'}
+    solution_star = create :solution_star, solution: solution, user: @current_user
+    post my_starred_solutions_path, params: {solution_id: solution.uuid, format: 'js'}
 
     assert_response :success
     assert_equal 0, StarredSolution.where(solution: solution).size
