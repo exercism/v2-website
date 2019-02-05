0f1586cf407abed8925cc88aa50d0ed663439b28
Jeremy Walker <jez.walker@gmail.com>
2019-02-05 12:49:10 -0800

    Fix bug with double clicking on leave button

--- test/controllers/my/user_tracks_controller_test.rb
+++ test/controllers/my/user_tracks_controller_test.rb
@@ -39,4 +39,19 @@ class UserTracksControllerTest < ActionDispatch::IntegrationTest
     SwitchTrackToMentoredMode.expects(:call).with(@current_user, track)
     patch set_mentored_mode_my_user_track_url(user_track)
   end
+
+  test "#leave removes track and redirects" do
+    sign_in!
+    user_track = create :user_track, user: @current_user
+
+    delete leave_my_user_track_url(user_track)
+    assert_redirected_to my_tracks_path
+    refute UserTrack.where(id: user_track.id).exists?
+  end
+
+  test "#leave redirects to tracks page if track doesn't exist" do
+    sign_in!
+    delete leave_my_user_track_url(123)
+    assert_redirected_to my_tracks_path
+  end
 end
