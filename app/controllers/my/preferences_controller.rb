0584c0df0c9de7de597ceb9dbf0e3ab97982b502
Jeremy Walker <jez.walker@gmail.com>
2019-03-03 14:11:59 +0000

    Add setting to allow disabling of one-click unsubscribes

--- app/controllers/my/preferences_controller.rb
+++ app/controllers/my/preferences_controller.rb
@@ -13,6 +13,13 @@ class My::PreferencesController < MyController
     elsif params[:update_dark_theme].present?
       current_user.update(dark_code_theme: (params[:dark_code_theme] == "1"))
       redirect_to({action: :edit}, notice: "Code theme preference updated successfully")
+    elsif params[:update_require_password_to_update_communication_preferences].present?
+      if params[:require_password_to_update_communication_preferences] == "1"
+        current_user.communication_preferences.update(token: nil)
+      else
+        current_user.communication_preferences.update(token: SecureRandom.uuid)
+      end
+      redirect_to({action: :edit}, notice: "Password preference updated successfully")
     else
       redirect_to({action: :edit})
     end
