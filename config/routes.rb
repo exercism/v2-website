41affd7e0dc323807fdeba22fdd1efa897020b89
Jeremy Walker <jez.walker@gmail.com>
2019-03-21 21:21:20 +0000

    Enable sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   # ### #
   # API #
   # ### #
