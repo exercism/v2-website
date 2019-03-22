b7cd2709a9bf77ed67e7db770cbab39d1e723b7e
Jeremy Walker <jez.walker@gmail.com>
2019-03-22 16:41:30 +0000

    Add propono config values

--- config/initializers/propono.rb
+++ config/initializers/propono.rb
@@ -1,18 +1,12 @@
 module Propono
   def self.configure_client
     Client.new do |config|
-      creds = YAML::load(ERB.new(File.read("#{Rails.root}/config/propono.yml.erb")).result).stringify_keys
-      creds = (creds[Rails.env]).symbolize_keys
-
-      if creds[:use_iam_profile]
-        config.use_iam_profile = true
-      else
-        config.access_key = creds[:access_key]
-        config.secret_key = creds[:secret_key]
-      end
-      config.queue_region = creds[:queue_region]
-      config.queue_suffix = creds[:queue_suffix] || ""
-      config.application_name = creds[:application_name]
+      config.access_key = Rails.application.secrets.aws_access_key_id
+      config.secret_key = Rails.application.secrets.aws_secret_access_key
+      config.queue_region = Rails.application.secrets.aws_region
+      config.queue_suffix = Rails.env.production?? "" : "-#{Rails.env}_#{`whoami`}"
+      config.application_name = Rails.env.production?? "exercism_website" :
+                                                       "exercism_website_#{Rails.env}_#{`whoami`}"
       config.logger = Rails.logger
     end
   end
