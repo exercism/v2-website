10c6ad1dc9d8e2a35c32552c57fc0b7e1ef25bfd
Jeremy Walker <jez.walker@gmail.com>
2019-03-22 14:05:55 +0000

    Daemonise DJ and remove Sidekiq

--- config/initializers/action_job.rb
+++
@@ -1,8 +0,0 @@
-=begin
-Rails.application.config.active_job.queue_adapter = :sidekiq
-Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.secrets.redis_url } if Rails.env.production?
-end
-=end
-
-#Rails.application.config.active_job.queue_adapter = :delayed_job
