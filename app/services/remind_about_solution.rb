61f560e72ae15995ca33f381bab27d205662fd0d
Jeremy Walker <jez.walker@gmail.com>
2019-04-05 00:27:26 +0100

    Fix broken headings

--- app/services/remind_about_solution.rb
+++ app/services/remind_about_solution.rb
@@ -9,14 +9,14 @@ class RemindAboutSolution
     return if solution.last_updated_by_user_at.to_i > solution.last_updated_by_mentor_at.to_i
     return if email_log.remind_about_solution_sent_at.to_i > (Time.current - 1.day).to_i
 
-    solution.update(reminder_sent_at: Time.current)
-
     DeliverEmail.(
       solution.user,
       :remind_about_solution,
       solution,
       other_solutions
     )
+
+    solution.update(reminder_sent_at: Time.current)
   end
 
   private
@@ -24,7 +24,8 @@ class RemindAboutSolution
     solution.user.solutions.
       where.not(id: solution.id).
       not_completed.
-      where("last_updated_by_mentor_at > last_updated_by_user_at")
+      where("last_updated_by_mentor_at > last_updated_by_user_at").
+      to_a
   end
 
   memoize
