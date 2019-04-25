5ff19a3d7c3763251b5c6a80efa6f8c4677a7fa3
Jeremy Walker <jez.walker@gmail.com>
2019-04-25 13:35:31 +0100

    Don't setup mentor_requested_at if there are no iterations

--- test/services/unlock_next_core_exercise_test.rb
+++ test/services/unlock_next_core_exercise_test.rb
@@ -18,7 +18,25 @@ class UnlockNextCoreExerciseTest < ActiveSupport::TestCase
     assert_equal next_solution, UnlockNextCoreExercise.(track, user)
   end
 
-  test "returns existing solution if it exists and sets mentoring_requested_at" do
+  test "if existing solution exists set mentoring_requested_at and return" do
+    Timecop.freeze do
+      track = create(:track)
+      user = create(:user)
+      next_core_exercise = create(:exercise, track: track, core: true, position: 2)
+      next_core_solution = create(:solution,
+             user: user,
+             exercise: next_core_exercise,
+             completed_at: nil,
+             mentoring_requested_at: nil)
+      create(:iteration, solution: next_core_solution)
+      UnlockCoreExercise.expects(:call).with(user, next_core_exercise).never
+
+      assert_equal next_core_solution, UnlockNextCoreExercise.(track, user)
+      assert_equal Time.current.to_i, next_core_solution.reload.mentoring_requested_at.to_i
+    end
+  end
+
+  test "if existing solution don't set mentoring_requested_at if no iterations" do
     Timecop.freeze do
       track = create(:track)
       user = create(:user)
@@ -31,7 +49,7 @@ class UnlockNextCoreExerciseTest < ActiveSupport::TestCase
       UnlockCoreExercise.expects(:call).with(user, next_core_exercise).never
 
       assert_equal next_core_solution, UnlockNextCoreExercise.(track, user)
-      assert_equal Time.current.to_i, next_core_solution.reload.mentoring_requested_at.to_i
+      assert_nil next_core_solution.reload.mentoring_requested_at
     end
   end
 
