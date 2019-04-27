0a03685a2ab753781d2e19d0cbf04279e1ee33b7
Jeremy Walker <jez.walker@gmail.com>
2019-04-27 20:07:08 +0100

    New mentor heartbeat

--- app/services/generate_mentor_heartbeats.rb
+++ app/services/generate_mentor_heartbeats.rb
@@ -9,7 +9,7 @@ class GenerateMentorHeartbeats
       if email_log.mentor_heartbeat_sent_at.nil?
         introduction = %Q{Welcome to your first Mentor Heartbeat!\n\nEach week, we'll be sending you an email that summarises the activity on each track you're mentoring. We'll also include information on any changes or updates to the mentoring side of Exercism. If you have any ideas on what you'd like to see here, please open an issue at on GitHub and let us know your thoughts. If you want to opt out, there's a link at the bottom of the email. Finally I just want to say a huge thank you for your hard work and for the thousands of people you're all helping on Exercism!}
       else
-        introduction = %Q{We get a lot of questions in Slack and on GitHub about what Exercism's current priorities are and why we've made those decisions. To try and help be more clear and transparent about this we've added a strategy document to the website, which lays out what we're trying to achieve in 2019 and how we're progressing. You might like to have a read: https://exercism.io/strategy}
+        introduction = %Q{This week, we'd like to remind people about the mentors' Slack workspace. It's a great place to discuss solutions that you'd like a second opinion on, improve your mentoring skills, and get to know other mentors and enjoy the community vibe. We also post nice feedback from students in the #making-a-difference channel. Many people join up, say hello, then forget about Slack, so we thought we'd send a friendly reminder in case you'd forgotten about it and are missing out!}
       end
 
       merged_tracks_stats = {}
