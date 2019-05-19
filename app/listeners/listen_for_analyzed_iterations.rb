de36bfe5698b856aadb5a9ca4adc2ff8a4ccbd76
Jeremy Walker <jez.walker@gmail.com>
2019-05-19 17:35:26 +0100

    Add handling of auto analysis responses

--- app/listeners/listen_for_analyzed_iterations.rb
+++ app/listeners/listen_for_analyzed_iterations.rb
@@ -3,8 +3,17 @@ class ListenForAnalyzedIterations
 
   def call
     client.listen(:iteration_analyzed) do |message|
-      puts message[:iteration_id]
-      puts message
+      iteration_id = message[:iteration_id]
+      status = message[:status]
+      analysis = message[:analysis]
+
+      iteration = Iteration.find_by_id(iteration_id)
+      if iteration
+        p "Handling iteration analysis: #{iteration_id}"
+        HandleIterationAnalysis.(iteration, status, analysis)
+      else
+        p "No iteration: #{iteration_id}"
+      end
     end
   end
 
