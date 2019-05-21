099f9dc11965d61ef2507bb363f9b535a6b535f1
Jeremy Walker <jez.walker@gmail.com>
2019-05-21 13:44:11 +0100

    Switch order on analyses

--- app/controllers/mentor/analyses_controller.rb
+++ app/controllers/mentor/analyses_controller.rb
@@ -1,7 +1,8 @@
 class Mentor::AnalysesController < MentorController
   def index
     @analyses = IterationAnalysis.includes(iteration: {solution: {exercise: :track}}).
-                                           page(params[:page]).per(20)
+                                  order('id DESC').
+                                  page(params[:page]).per(20)
   end
 
   def show
