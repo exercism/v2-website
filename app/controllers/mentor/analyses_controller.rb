a94a72a94767468a406c65f1fa2fc5ef90f74d1d
Jeremy Walker <jez.walker@gmail.com>
2019-06-25 16:23:34 +0100

    Add filter for mentor analyses

--- app/controllers/mentor/analyses_controller.rb
+++ app/controllers/mentor/analyses_controller.rb
@@ -1,8 +1,13 @@
 class Mentor::AnalysesController < MentorController
   def index
+    @tracks = Track.where(id: IterationAnalysis.joins(iteration: {person_solution: :exercise}).select("exercises.track_id"))
+
+    @track_id = params[:track_id]
     @analyses = IterationAnalysis.includes(iteration: {solution: {exercise: :track}}).
-                                  order('id DESC').
-                                  page(params[:page]).per(20)
+                                  order('id DESC')
+
+    @analyses = @analyses.joins(iteration: {person_solution: :exercise}).where('exercises.track_id': @track_id) if @track_id.to_i > 0
+    @analyses = @analyses.page(params[:page]).per(20)
   end
 
   def show
