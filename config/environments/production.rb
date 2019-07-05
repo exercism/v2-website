979a630ccd6577f51841c0fb84455a20d0c3567e
Jeremy Walker <jez.walker@gmail.com>
2019-07-05 20:58:48 +0100

    Change logging to be called during Rails boot sequence

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # Setup logging
+  Exercism::configure_logging(config)
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
