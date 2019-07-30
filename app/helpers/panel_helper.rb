7e815104fd71a7a5a4744ff62c7db3f1b2c3f083
Karlo Soriano <karlo@thalamus.ai>
2019-07-30 13:12:06 +0800

    Move from partial to helper

--- app/helpers/panel_helper.rb
+++ app/helpers/panel_helper.rb
@@ -1,5 +1,17 @@
 module PanelHelper
-  def panel_classes_for(user)
-    "panels--vertical-split" unless user.full_width_code_panes?
+  def render_panels(user = current_user, &block)
+    options = { vertical_split: !user.full_width_code_panes? }
+
+    content_tag :div, capture(&block), class: panel_classes(options)
+  end
+
+  private
+
+  def panel_classes(vertical_split:)
+    classes = ["panels"]
+
+    classes << "panels--vertical-split" if vertical_split
+
+    classes.join(" ")
   end
 end
