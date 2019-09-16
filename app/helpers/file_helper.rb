cb7a3f40070ff4a40927558d9786e81e5bf8412e
Simon Shine <shreddedglory@gmail.com>
2019-09-16 08:43:13 +0200

    Rewrite FileHelper.syntax_highlighter_for_filename according to feedback

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -1,13 +1,8 @@
 module FileHelper
   def syntax_highlighter_for_filename(filename, track)
-    parts = filename.split(".")
-
-    if !parts.empty?
-      extension = parts.last.downcase
-      lang = Exercism::PrismFileMappings[extension]
-    end
-
-    lang ||= track.syntax_highligher_language
+    extension = filename.split(".").last.try(&:downcase)
+    lang = Exercism::PrismFileMappings[extension] ||
+           track.syntax_highligher_language
 
     "language-#{lang}"
   end
