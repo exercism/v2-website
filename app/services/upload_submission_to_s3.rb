8690a966a24c11e07c267a64bbac65c7c488f574
Jeremy Walker <jez.walker@gmail.com>
2019-09-20 19:15:14 +0100

    Upload source code as well as user code for submissions

--- app/services/upload_submission_to_s3.rb
+++ app/services/upload_submission_to_s3.rb
@@ -1,19 +1,57 @@
 class UploadSubmissionToS3
   include Mandate
 
-  initialize_with :submission
+  def initialize(submission)
+    @submission = submission
+    @path = "#{Rails.env}/submissions/#{submission.id}"
+    @solution = submission.solution
+    @track = solution.exercise.track
+    @uploaded_files = []
+  end
 
   def call
-    path = "#{Rails.env}/submissions/#{submission.id}"
+    upload_submission_files
+    upload_exercise_files
+  end
+
+  private
+  attr_reader :path, :solution, :submission, :track, :uploaded_files
+
+  def upload_submission_files
     submission.files.each do |file|
+      next if file.filename.with_slashes =~ Regexp.new(track.repo.test_pattern)
+      next if uploaded_files.include?(file.filename.with_slashes)
+
       key = "#{path}/#{file.filename.with_slashes}"
       s3_client.put_object(body: file.download,
                            bucket: submissions_bucket,
                            key: key,
                            acl: 'private')
+
+      uploaded_files << file.filename.with_slashes
     end
   end
 
+  def upload_exercise_files
+    exercise_slug = solution.git_slug
+    track_url = track.repo_url
+
+    exercise_reader = Git::ExercismRepo.new(track_url).exercise(exercise_slug, solution.git_sha)
+    exercise_reader.files.each do |filepath|
+      next if filepath =~ track.repo.ignore_regexp
+      next if uploaded_files.include?(filepath)
+
+      key = "#{path}/#{filepath}"
+      body = exercise_reader.read_file(filepath)
+
+      s3_client.put_object(body: body,
+                           bucket: submissions_bucket,
+                           key: key,
+                           acl: 'private')
+
+      uploaded_files << filepath
+    end
+  end
 
   def s3_client
     @client ||= Aws::S3::Client.new(
