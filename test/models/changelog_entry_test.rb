f72ba471ad0e56a0c8957bb8795dead7d0370858
Karlo Soriano <karlo@thalamus.ai>
2019-09-25 10:31:43 +0800

    Raise an error when publishing an already published entry

--- test/models/changelog_entry_test.rb
+++ test/models/changelog_entry_test.rb
@@ -10,6 +10,15 @@ class ChangelogEntryTest < ActiveSupport::TestCase
     assert_equal time, entry.published_at
   end
 
+  test "#publish! raises an error when publishing an already published entry" do
+    time = Time.utc(2016, 12, 25)
+    entry = create(:changelog_entry, published_at: time)
+
+    assert_raises ChangelogEntry::EntryAlreadyPublishedError do
+      entry.publish!
+    end
+  end
+
   test "published? returns true if published_at is set" do
     entry = create(:changelog_entry, published_at: Time.new(2016, 12, 25))
 
