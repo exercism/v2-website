d203a26c1dbfaf193089ede43917cbed32dc9ba6
Karlo Soriano <karlo@thalamus.ai>
2019-09-25 08:18:54 +0800

    Use exact_text option correctly

--- test/support/selectize_helpers.rb
+++ test/support/selectize_helpers.rb
@@ -2,7 +2,7 @@ module SelectizeHelpers
   def select_option(option, selector:)
     within("select#{selector}+.selectize-control") do
       first('div.selectize-input').click
-      find('div.option', text: option, exact_text: true).click
+      find('div.option', exact_text: option).click
     end
   end
 end
