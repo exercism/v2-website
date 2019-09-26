539cdf0563ef5469c6651ece11b775838a282a40
Karlo Soriano <karlo@thalamus.ai>
2019-09-26 15:36:51 +0800

    Rewrite stubs in view test to be less coupled

--- test/views/changelog_admin/entries/actions_test.rb
+++ test/views/changelog_admin/entries/actions_test.rb
@@ -3,9 +3,8 @@ require "test_helper"
 module ChangelogAdmin
   class ActionsTest < ActionView::TestCase
     test "show publish button if entry is unpublished" do
-      user = create(:user)
       entry = create(:changelog_entry, published_at: nil)
-      view.stubs(:current_user).returns(user)
+      view.stubs(:allowed_to_edit_changelog_entry?)
 
       render "changelog_admin/entries/actions", entry: entry
 
@@ -13,20 +12,16 @@ module ChangelogAdmin
     end
 
     test "hide publish button if entry is published" do
-      user = create(:user)
       entry = create(:changelog_entry, published_at: Time.new(2016, 12, 25))
-      view.stubs(:current_user).returns(user)
+      view.stubs(:allowed_to_edit_changelog_entry?)
 
       render "changelog_admin/entries/actions", entry: entry
 
       assert_select "a", text: "Publish", exact: true, count: 0
-
     end
 
     test "show edit button if entry is editable by user" do
-      user = create(:user)
-      entry = create(:changelog_entry, created_by: user)
-      view.stubs(:current_user).returns(user)
+      entry = create(:changelog_entry)
       view.
         stubs(:allowed_to_edit_changelog_entry?).
         with(entry).
@@ -38,10 +33,7 @@ module ChangelogAdmin
     end
 
     test "hide edit button if entry is not editable by user" do
-      user = create(:user)
-      other_user = create(:user)
-      entry = create(:changelog_entry, created_by: other_user)
-      view.stubs(:current_user).returns(user)
+      entry = create(:changelog_entry)
       view.
         stubs(:allowed_to_edit_changelog_entry?).
         with(entry).
