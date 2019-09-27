759b6b923c9b037f7859a6cca7de223ca20d75f8
Karlo Soriano <karlo@thalamus.ai>
2019-09-27 16:00:33 +0800

    Add unique link names based on info_url

--- app/helpers/changelog_entry_helper.rb
+++ app/helpers/changelog_entry_helper.rb
@@ -9,4 +9,17 @@ module ChangelogEntryHelper
       user: user
     )
   end
+
+  def changelog_entry_info_url_text(info_url)
+    case info_url
+    when /https:\/\/github.com\/.*\/.*\/pull/
+      "View PR on GitHub"
+    when /https:\/\/github.com\/.*\/.*\/commit/
+      "View commit on GitHub"
+    when /https:\/\/github.com\/.*\/.*\/issues/
+      "View issue on GitHub"
+    else
+      "View"
+    end
+  end
 end
