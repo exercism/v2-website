4afb2b2c7a5548621919c06cbfffd277d73e0b79
Karlo Soriano <karlo@thalamus.ai>
2019-10-07 19:30:02 +0800

    Make icons more accessible

--- app/helpers/a11y_helper.rb
+++ app/helpers/a11y_helper.rb
@@ -1,11 +1,15 @@
 # Accessibilty aka a11y helpers
 module A11yHelper
-  def icon(type, label = nil)
-    content_tag :i, '', class: "fa fa-#{type}", 'aria-label': (label || type)
+  def icon(type, label = nil, style: :normal, fixed_width: false)
+    base_class = font_awesome_class(style, fixed_width)
+
+    content_tag :i, '', class: "#{base_class} fa-#{type}", 'aria-label': (label || type)
   end
 
-  def graphical_icon(type)
-    content_tag :i, '', class: "fa fa-#{type}", 'aria-hidden': true
+  def graphical_icon(type, style: :normal, fixed_width: false)
+    base_class = font_awesome_class(style, fixed_width)
+
+    content_tag :i, '', class: "#{base_class} fa-#{type}", 'aria-hidden': true
   end
 
   def graphical_image(source, options = {})
@@ -14,5 +18,19 @@ module A11yHelper
 
   def image(source, alt, options = {})
     image_tag source, { alt: alt }.merge(options)
+  end
+
+  private
+
+  def font_awesome_class(style)
+    style_class = case style
+                  when :normal
+                    "fa"
+                  when :light
+                    "fal"
+                  end
+    fixed_width_class = fixed_width ? "fa-fw" : ""
+
+    [style_class, fixed_width_class].join(" ")
   end
 end
