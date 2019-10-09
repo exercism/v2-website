ca8d9f0e8406dba69e9eac191c2687b2a646a66b
Karlo Soriano <karlo@thalamus.ai>
2019-10-09 12:46:33 +0800

    Generalize mentor feedback request button state for oversubscribed tracks

---
+++ app/models/mentor_feedback_request.rb
@@ -0,0 +1,34 @@
+class MentorFeedbackRequest
+  attr_reader :solution
+  attr_accessor :status
+
+  delegate(
+    :track_max_mentoring_slots,
+    :track_mentoring_slots_remaining,
+    to: :solution
+  )
+
+  def initialize(solution, status: nil)
+    @solution = solution
+    @status = status || solution_status
+  end
+
+  private
+  delegate :user_track, to: :solution
+
+  def solution_status
+    return :oversubscribed unless solution.track_accepting_new_students?
+
+    if solution.exercise_is_core? &&
+        user_track.mentored_mode? &&
+        !solution.last_updated_legacy?
+      return :promoted_to_core
+    end
+
+    if user_track.mentoring_slots_remaining?
+      :mentoring_slots_remaining
+    else
+      :mentoring_slots_used
+    end
+  end
+end
