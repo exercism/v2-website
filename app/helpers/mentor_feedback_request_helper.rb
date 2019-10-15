904c2edf471b3be6de13d0ee25f6e909281bdcdb
Karlo Soriano <karlo@thalamus.ai>
2019-10-16 07:29:28 +0800

    Add tests to mentor feedback request helper and remove unused partials

--- app/helpers/mentor_feedback_request_helper.rb
+++ app/helpers/mentor_feedback_request_helper.rb
@@ -1,6 +1,6 @@
 module MentorFeedbackRequestHelper
-  def render_solution_mentor_feedback_request_section(scenario, solution)
-    mentor_request = MentorFeedbackRequest.new(solution)
+  def render_solution_mentor_feedback_request_section(scenario, solution, mentor_request = nil)
+    mentor_request ||= MentorFeedbackRequest.new(solution)
 
     render "my/solutions/mentor_feedback_request/#{scenario}/#{mentor_request.status}", mentor_request: mentor_request
   end
