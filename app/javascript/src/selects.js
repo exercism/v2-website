44cd3da8d74897d5d9c0855ca1f00635cbe38633
Karlo Soriano <karlo@thalamus.ai>
2019-10-23 06:50:35 +0800

    Add different handler for forms submitted via HTML and JS

--- app/javascript/src/selects.js
+++ app/javascript/src/selects.js
@@ -1,5 +1,12 @@
 window.submitOnChange = function($select) {
   $select.change(function() {
-    Rails.fire($(this).parents('form')[0], 'submit')
+    var form = $(this).parents('form');
+    var isRemote = $(form).data("remote");
+
+    if (isRemote) {
+      Rails.fire(form[0], 'submit');
+    } else {
+      form[0].submit();
+    }
   })
 }
