2c6a02edad29a62e7d81f0817c41f7113b3939e0
Karlo Soriano <karlo@thalamus.ai>
2019-11-13 16:58:12 +0800

    Use selectors that wait for AJAX to finish

--- test/support/selectize_helpers.rb
+++ test/support/selectize_helpers.rb
@@ -6,10 +6,11 @@ module SelectizeHelpers
       end
     end
   end
+
   def select_option(option, selector:)
     within("select#{selector}+.selectize-control") do
-      first('div.selectize-input').click
-      find('div.option', exact_text: option).click
+      find('div.selectize-input').click
+      find('div.option', text: option).click
     end
   end
 end
