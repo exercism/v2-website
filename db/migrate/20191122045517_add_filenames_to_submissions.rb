83f97db0ea7af82f137bf6b51484539937498a7f
Jeremy Walker <jez.walker@gmail.com>
2019-11-27 16:02:24 +0900

    Fix migration

--- db/migrate/20191122045517_add_filenames_to_submissions.rb
+++ db/migrate/20191122045517_add_filenames_to_submissions.rb
@@ -1,7 +1,7 @@
 class AddFilenamesToSubmissions < ActiveRecord::Migration[6.0]
   def change
-    add_column :submissions, :filenames, :json, null: true
-    Submission.update_all(filenames: [])
+    #add_column :submissions, :filenames, :json, null: true
+    #Submission.update_all(filenames: [])
     change_column_null :submissions, :filenames, false
   end
 end
