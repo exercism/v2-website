d3530956a67715dc6fa197a59a772c1ce60b6489
Karlo Soriano <karlo@thalamus.ai>
2019-11-27 15:40:47 +0900

    Add test_runner_orchestrator_url to secrets

--- app/services/submission_services/run_tests.rb
+++ app/services/submission_services/run_tests.rb
@@ -5,11 +5,17 @@ module SubmissionServices
     initialize_with :uuid, :solution
 
     def call
-      RestClient.post 'http://localhost:9292/submissions', {
+      RestClient.post "#{orchestrator_url}/submissions", {
         track_slug: solution.exercise.track.slug,
         exercise_slug: solution.exercise.slug,
         submission_uuid: uuid
       }
+    end
+
+    private
+
+    def orchestrator_url
+      Rails.application.secrets.test_runner_orchestrator_url
     end
   end
 end
