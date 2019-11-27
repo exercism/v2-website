8b8a23245aeb74b72d9aedcad233fe0d75a28e1a
Karlo Soriano <karlo@thalamus.ai>
2019-11-27 15:32:05 +0900

    Move SubmissionTest class into SubmissionTestResults

--- app/models/submission_test_results.rb
+++ app/models/submission_test_results.rb
@@ -24,4 +24,17 @@ class SubmissionTestResults < ApplicationRecord
   def failed_tests
     tests.select(&:failed?)
   end
+
+  class SubmissionTest
+    attr_reader :name, :status, :message
+    def initialize(params)
+      @name = params["name"]
+      @status = params["status"]
+      @message = params["message"]
+    end
+
+    def failed?
+      status == "fail"
+    end
+  end
 end
