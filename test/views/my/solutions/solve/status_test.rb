78e300c5cc54b1a40f4fbe648de5c41bbb6e30c9
Karlo Soriano <karlo@thalamus.ai>
2019-11-27 15:42:53 +0900

    Remove unused test

--- test/views/my/solutions/solve/status_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class StatusTest < ActionView::TestCase
-  test "renders nothing when submission is nil" do
-    render "my/solutions/solve/status", submission: nil
-
-    assert_empty rendered
-  end
-end
