8b1da4a45b41ffe9383011e3de33890a3527c03c
Jeremy Walker <jez.walker@gmail.com>
2019-11-27 16:04:35 +0900

    Readd migration

--- db/migrate/20191122045517_add_filenames_to_submissions.rb
+++ db/migrate/20191122045517_add_filenames_to_submissions.rb
@@ -1,6 +1,6 @@
 class AddFilenamesToSubmissions < ActiveRecord::Migration[6.0]
   def change
-    #add_column :submissions, :filenames, :json, null: true
+    add_column :submissions, :filenames, :json, null: true
     #Submission.update_all(filenames: [])
     change_column_null :submissions, :filenames, false
   end
