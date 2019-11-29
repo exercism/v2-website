41c629440c2767e18a3cc52f69ef8ee9d55f1fa4
Karlo Soriano <karlo@thalamus.ai>
2019-11-29 16:37:15 +0900

    Allow user to sign up via the research website

--- app/controllers/research/sessions_controller.rb
+++ app/controllers/research/sessions_controller.rb
@@ -2,10 +2,8 @@ module Research
   class SessionsController < ::SessionsController
     def create
       super { |user| user.join_research! }
-    end
 
-    def after_sign_in_path_for(resource)
-      session["return_to"] || research_dashboard_path
+      session["return_to"] ||= research_dashboard_path
     end
   end
 end
