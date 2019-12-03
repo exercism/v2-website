8ab85285fba1f7eec3615eaf5b90a6cff1d63c23
Karlo Soriano <karlo@thalamus.ai>
2019-12-03 16:49:40 +0900

    Style status pages for submission

--- app/javascript/src/submission_status_view.js
+++ app/javascript/src/submission_status_view.js
@@ -6,56 +6,58 @@ class SubmissionStatusView {
   render() {
     switch(this.status) {
       case 'queueing':
-        return `<div class="submission-status">
+        return `<div class="submission-status-queueing">
           <div class="submission-status-spinner">
             <i aria-hidden="true" class="fas fa-spinner fa-spin"></i>
           </div>
           <p class="submission-status-title">Processing</p>
-          <p class="submission-status-status">Queueing</p>
+          <p class="submission-status-status">Queueing...</p>
         </div>`
       case 'queued':
-        return `<div class="submission-status">
+        return `<div class="submission-status-queueing">
           <div class="submission-status-spinner">
             <i aria-hidden="true" class="fas fa-spinner fa-spin"></i>
           </div>
           <p class="submission-status-title">Processing</p>
-          <p class="submission-status-status">Queued</p>
+          <p class="submission-status-status">Queued...</p>
         </div>`
       case 'tested':
-        return `<div class="submission-status">
+        return `<div class="submission-status-tested">
           <div class="submission-status-spinner">
-            <i aria-hidden="true" class="fas fa-check-circle"></i>
+            <i aria-hidden="true" class="fal fa-check-circle"></i>
           </div>
           <p class="submission-status-title">Finished</p>
-          <p class="submission-status-status">Tests Complete</p>
+          <p class="submission-status-status">Tests complete</p>
         </div>`
       case 'timeout':
-        return `<div class="submission-status">
+        return `<div class="submission-status-timeout">
           <div class="submission-status-spinner">
-            <i aria-hidden="true" class="fas fa-ellipsis-h"></i>
+            <i aria-hidden="true" class="fal fa-ellipsis-h"></i>
           </div>
           <p class="submission-status-title">Taking too long?</p>
           <p class="submission-status-status">
             It looks like you've been waiting a while. Re-run your code and
             we'll boost it to the front of the queue.
           </p>
-          <button class="js-submit-code pure-button pure-button-primary">
+          <button class="submission-status-button js-submit-code pure-button pure-button-primary">
             Re-run your code
           </button>
         </div>`
       case 'error':
         return `<div class="submission-error">
-          <p class="submission-error-title">Sorry</p>
-          <p class="submission-error-message">
-            Uh oh, it looks like our servers aren't working and we can't run
-            your code. Rest assured your work has been saved.
-            <br />
-            <br />
-            Please try to run your code again in a few minutes.
-          <button class="js-overlay-close pure-button pure-button-primary">
-            Continue
-          </button>
-          </p>
+          <div class="submission-error-content">
+            <p class="submission-error-title">Sorry</p>
+            <p class="submission-error-message">
+              Uh oh, it looks like our servers aren't working and we can't run
+              your code. Rest assured your work has been saved.
+              <br />
+              <br />
+              Please try to run your code again in a few minutes.
+            </p>
+            <button class="js-overlay-close pure-button pure-button-primary submission-error-button">
+              Continue
+            </button>
+          </div>
         </div>`
     }
   }
