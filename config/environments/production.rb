b62265a25943b3c098499f0bcc3d8d62e34715d9
Karlo Soriano <karlo@thalamus.ai>
2019-12-03 17:53:28 +0900

    Add research.exercism.io to request origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ "https://exercism.io" ]
+  config.action_cable.allowed_request_origins = [ "https://exercism.io", "https://research.exercism.io" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
