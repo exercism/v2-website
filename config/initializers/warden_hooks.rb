cde6b55633da14623335a7082de9fa4f3d6c4659
Karlo Soriano <karlo@thalamus.ai>
2019-12-04 17:10:52 +0900

    Secure websockets

---
+++ config/initializers/warden_hooks.rb
@@ -0,0 +1,9 @@
+Warden::Manager.after_set_user do |user,auth,opts|
+  scope = opts[:scope]
+  auth.cookies.signed["#{scope}.id"] = user.id
+end
+
+Warden::Manager.before_logout do |user, auth, opts|
+  scope = opts[:scope]
+  auth.cookies.signed["#{scope}.id"] = nil
+end
