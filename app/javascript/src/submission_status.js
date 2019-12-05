40bfd5c30c9c0d76bdc8a4e757178618f399a0a0
Karlo Soriano <karlo@thalamus.ai>
2019-12-05 18:17:14 +0900

    Allow user to cancel a build via the escape button

--- app/javascript/src/submission_status.js
+++ app/javascript/src/submission_status.js
@@ -3,16 +3,21 @@ import TimeoutTimer from './timeout_timer';
 import Overlay from './overlay';
 
 class SubmissionStatus {
-  constructor(onRender, onTested) {
-    this.timeout = 30;
-    this.timer = new TimeoutTimer(this.timeout, () => {
-      this.setStatus('timeout');
+  constructor() {
+    this.timer = new TimeoutTimer(1, () => { this.setStatus('timeout'); });
+
+    this._setupShortcuts();
+  }
+
+  _setupShortcuts() {
+    $('body').keydown((e) => {
+      if(e.key === 'Escape') { this._cancelBuild(); }
     });
-    this.onRender = onRender;
-    this.onTested = onTested;
   }
 
   setStatus(status) {
+    this.status = status;
+
     this.timer.reset();
 
     switch(status) {
@@ -39,15 +44,37 @@ class SubmissionStatus {
 
         break;
       }
-    }
+      case 'cancelled': {
+        new Overlay().close();
 
-    this.onRender(new Overlay().getOverlay());
+        break;
+      }
+    }
   }
 
   _renderOverlay(status) {
     const html = new SubmissionStatusView(status).render();
 
     new Overlay().show(html);
+    this._setupActions(new Overlay().getOverlay());
+  }
+
+  _setupActions(container) {
+    container.find('.js-submit-code').click(this.onSubmit.bind(this));
+    container.find('.js-cancel-submission').click(this._cancelBuild.bind(this));
+  }
+
+  _cancelBuild() {
+    if (this.status !== 'queueing' && this.status !== 'queued') {
+      return;
+    }
+
+    const cancel = confirm("Are you sure you want to cancel this build?");
+
+    if (cancel) {
+      this.setStatus('cancelled');
+      this.onCancel();
+    }
   }
 }
 
