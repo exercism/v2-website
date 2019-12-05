d7413bf5105f1851e87b4d5220ae93b611fcbc07
Jeremy Walker <jez.walker@gmail.com>
2019-12-05 18:19:17 +0900

    Fix rebase issues

--- test/controllers/spi/submission_test_runs_controller_test.rb
+++ test/controllers/spi/submission_test_runs_controller_test.rb
@@ -1,12 +1,12 @@
 require_relative './test_base'
 
 class SPI::SubmissionTestRunControllerTest < SPI::TestBase
-   test "creates submission test run" do
+   test "creates submission test run on success" do
     submission = create :submission
     ops_status = 200
     results = {"foo" => "all good things"}
 
-    SubmissionServices::ProcessTestRun.expects(:call).with(submission, ops_status, ops_message, results)
+    SubmissionServices::ProcessTestRun.expects(:call).with(submission, ops_status, nil, results)
 
     post spi_submission_test_runs_path(
         submission_uuid: submission.uuid,
@@ -26,9 +26,9 @@ class SPI::SubmissionTestRunControllerTest < SPI::TestBase
     ops_status = 200
     ops_message = "Something happened"
 
-    SubmissionServices::ProcessTestResults.expects(:call).with(submission, ops_status, ops_message, nil)
+    SubmissionServices::ProcessTestRun.expects(:call).with(submission, ops_status, ops_message, nil)
 
-    post spi_submission_test_results_path(
+    post spi_submission_test_runs_path(
         submission_uuid: submission.uuid,
       ), {
         ops_status: ops_status,
