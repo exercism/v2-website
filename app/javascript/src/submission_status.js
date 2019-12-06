5e6307bc10aeaeead4476f263b5c8e728cfe511a
Karlo Soriano <karlo@thalamus.ai>
2019-12-06 16:33:04 +0900

    Style cancel button

--- app/javascript/src/submission_status.js
+++ app/javascript/src/submission_status.js
@@ -1,6 +1,5 @@
 import SubmissionStatusView from './submission_status_view';
 import TimeoutTimer from './timeout_timer';
-import Overlay from './overlay';
 
 class SubmissionStatus {
   constructor() {
@@ -24,27 +23,16 @@ class SubmissionStatus {
 
         break;
       }
-    }
-  }
-
-  render(html) {
-    new Overlay().show(html);
-    this._onRender();
-  }
-
-  _onRender() {
-    this._setupActions(new Overlay().getOverlay());
-
-    switch(this.status) {
       case 'cancelled': {
-        new Overlay().close();
+        this.container.remove();
+        this.onCancel();
 
         break;
       }
       case 'tested': {
         setTimeout(
           () => {
-            new Overlay().close();
+            this.container.remove();
             this.onTested();
           },
           1000
@@ -55,15 +43,30 @@ class SubmissionStatus {
     }
   }
 
+  render(html) {
+    if (this.container) { this.container.remove(); }
+
+    this.container = $(html).appendTo('body');
+
+    this._setupActions();
+  }
+
   _setupShortcuts() {
     $('body').keydown((e) => {
       if(e.key === 'Escape') { this._cancelBuild(); }
     });
   }
 
-  _setupActions(container) {
-    container.find('.js-submit-code').click(this.onSubmit.bind(this));
-    container.find('.js-cancel-submission').click(this._cancelBuild.bind(this));
+  _setupActions() {
+    this.container.find('.js-submit-code').click(this.onSubmit.bind(this));
+    this.
+      container.
+      find('.js-cancel-submission').
+      click(this._cancelBuild.bind(this));
+    this.
+      container.
+      find('.js-overlay-close').
+      click(() => { this.container.remove() });
   }
 
   _cancelBuild() {
@@ -73,11 +76,7 @@ class SubmissionStatus {
 
     const cancel = confirm("Are you sure you want to cancel this build?");
 
-    if (cancel) {
-      this.setStatus('cancelled');
-      this.render();
-      this.onCancel();
-    }
+    if (cancel) { this.setStatus('cancelled'); }
   }
 }
 
