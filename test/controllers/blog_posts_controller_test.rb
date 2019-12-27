83ac7335903b80537d3ea054f4505d927e95c02f
Jeremy Walker <jez.walker@gmail.com>
2019-12-27 10:28:06 +0000

    Redirect to latest blog post slugs

--- test/controllers/blog_posts_controller_test.rb
+++ test/controllers/blog_posts_controller_test.rb
@@ -6,14 +6,26 @@ class BlogPostsControllerTest < ActionDispatch::IntegrationTest
     scheduled = create :blog_post, published_at: Time.current + 1.minute
 
     get blog_posts_path
+    assert_response :success
     assert_equal [published], assigns(:blog_posts)
   end
 
   test "show finds by slug" do
+    old_slug = "foobar"
+    new_slug = "barfoo"
+    blog_post = create :blog_post, slug: old_slug, published_at: Time.current - 1.minute
+    blog_post.update(slug: new_slug)
+    get blog_post_path(old_slug)
+
+    assert_redirected_to blog_post_path(new_slug)
+  end
+
+  test "show redirects to latest url" do
     blog_post = create :blog_post, published_at: Time.current - 1.minute
     get blog_post_path(blog_post.slug)
     assert_equal blog_post, assigns(:blog_post)
   end
+
 
   test "show 404s for unpublished post" do
     blog_post = create :blog_post, published_at: Time.current + 1.minute
