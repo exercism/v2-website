b074b29116c8c6b994186bf2f131992173ab4cc1
Jeremy Walker <jez.walker@gmail.com>
2020-01-14 11:24:15 +0000

    Make karlo core

--- app/services/sync_contributors.rb
+++ app/services/sync_contributors.rb
@@ -23,7 +23,7 @@ class SyncContributors
 
     Contributor.where(github_username: Maintainer.select(:github_username)).update_all(is_maintainer: true)
     Contributor.where(github_username: %w{
-      kytrinyx iHiD ccare nicolechalmers erikschierboom
+      kytrinyx iHiD ccare nicolechalmers kntsoriano erikschierboom
     }).update_all(is_core: true)
   end
 end
