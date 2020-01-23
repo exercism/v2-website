449a550bcffb667d96ee7fb4ca5f030058fddf1c
Jeremy Walker <jez.walker@gmail.com>
2020-01-23 15:06:59 +0000

    Fix weird rebase

--- test/models/terminal_output_test.rb
+++ test/models/terminal_output_test.rb
@@ -5,13 +5,10 @@ class TerminalOutputTest < ActiveSupport::TestCase
     assert_equal "ran@xub", TerminalOutput.new("ran@xub").to_html
   end
 
-<<<<<<< Updated upstream
-=======
   test "to_html parses ansi" do
     assert_equal "<span style='color:#A00;'>Hello</span><span style='color:#00A;'>World</span>", TerminalOutput.new("\e[31mHello\e[0m\e[34mWorld\e[0").to_html
   end
 
->>>>>>> Stashed changes
   test "to_html copes with nil" do
     assert_nil TerminalOutput.new(nil).to_html
   end
