e91a76012ca3a4e123f07702ac9d81f036402046
Karlo Soriano <karlo@thalamus.ai>
2020-01-24 17:18:23 +0800

    Set theme value based on user's theme

---
+++ test/system/research/user_updates_theme_test.rb
@@ -0,0 +1,17 @@
+require_relative "./test_case"
+
+class Research::UserUpdatesThemeTest < Research::TestCase
+  test "user updates theme" do
+    user = create(:user,
+                  :onboarded,
+                  joined_research_at: 2.days.ago,
+                  dark_code_theme: false)
+    solution = create(:research_experiment_solution, user: user)
+
+    sign_in!(user)
+    visit research_experiment_solution_path(solution)
+    select "Dark", from: "Theme:"
+
+    assert_css "body.prism-dark"
+  end
+end
