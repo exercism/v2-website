f825e302f4c112e0ba3f60c862b125f548dad86a
Jeremy Walker <jez.walker@gmail.com>
2020-01-29 15:37:05 +0000

    Add basic test run results skinning

--- app/models/submission_test_run_result.rb
+++ app/models/submission_test_run_result.rb
@@ -1,26 +1,34 @@
 class SubmissionTestRunResult
-  attr_reader :test_info, :name, :status, :message, :output
+  attr_reader :test_info, :name, :status, :message
 
   def initialize(test_info, params)
     @test_info = test_info
     @name = params["name"]
-    @status = params["status"]
+    @status = params["status"].to_sym
     @message = params["message"]
     @output = TerminalOutput.new(params["output"])
   end
 
   def output_html
-    output.to_html
+    @output_html ||= output.to_html
+  end
+
+  def passed?
+    status == :pass
   end
 
   def failed?
-    status == "fail"
+    status == :fail
   end
 
   def cmd
     return if test_info.blank?
 
     test_info.cmd
+  end
+
+  def expected
+    '"This is a stub"'
   end
 
   def text
@@ -36,6 +44,7 @@ class SubmissionTestRunResult
   end
 
   private
+  attr_reader :output
 
   def template
     test_info.msg
