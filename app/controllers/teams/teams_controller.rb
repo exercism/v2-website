4b12fde51d54b786dbef311162694bcf04c70b7b
Jeremy Walker <jez.walker@gmail.com>
2020-02-02 11:46:33 +0000

    Remove ability to invite to teams by email (#906)

--- app/controllers/teams/teams_controller.rb
+++ app/controllers/teams/teams_controller.rb
@@ -22,10 +22,10 @@ class Teams::TeamsController < Teams::BaseController
       params[:team][:avatar]
     )
 
-    emails = params[:emails].to_s.split("\n").map(&:strip).compact
-    emails.each do |email|
-      CreateTeamInvitation.(@team, current_user, email)
-    end
+    #emails = params[:emails].to_s.split("\n").map(&:strip).compact
+    #emails.each do |email|
+    #  CreateTeamInvitation.(@team, current_user, email)
+    #end
 
     redirect_to [:teams, @team]
   end
