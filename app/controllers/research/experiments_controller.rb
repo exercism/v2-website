4cf48322a3628491a0354a2177b78df90ab3f708
Jeremy Walker <jez.walker@gmail.com>
2020-03-26 14:50:05 +0000

    Fix flow of experiment with joining research

--- app/controllers/research/experiments_controller.rb
+++ app/controllers/research/experiments_controller.rb
@@ -1,15 +1,10 @@
 module Research
   class ExperimentsController < Research::BaseController
     skip_before_action :authenticate_user!
-    skip_before_action :check_user_joined_research!
+    #skip_before_action :check_user_joined_research!
 
     def index
       return redirect_to(action: :show, id: Experiment.first.id)
-
-      #@experiments = Experiment.all
-      #@user_experiments_by_ids = current_user.research_experiments.each_with_object({}) {|ue,h|
-      #  h[ue.experiment_id] = ue
-      #}
     end
 
     def show
