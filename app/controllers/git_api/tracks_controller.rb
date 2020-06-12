dd5bbd5edcd03c8c4a9d1729e630b08e180348a1
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-12 13:55:48 +0200

    Update app/controllers/git_api/tracks_controller.rb
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- app/controllers/git_api/tracks_controller.rb
+++ app/controllers/git_api/tracks_controller.rb
@@ -36,7 +36,7 @@ module GitAPI
           }
         end
         .select { |issue| filter_labels.all? { |filter_label| issue[:labels].include?(filter_label) } }
-        .sort { |a, b| a[:title] <=> b[:title] } 
+        .sort_by{|a|a[:title]}
     end
 
     def query(states)
