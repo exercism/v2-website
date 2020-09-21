a6e28ed0574908a76632cabd6474b7311aac7538
Jeremy Walker <jez.walker@gmail.com>
2020-09-21 20:38:38 +0100

    Don't use incorrect arg

--- app/controllers/admin/data/experiments_controller.rb
+++ app/controllers/admin/data/experiments_controller.rb
@@ -2,9 +2,7 @@ class Admin::Data::ExperimentsController < AdminController
   before_action :restrict_to_admins!
 
   def show
-    zip_path = ExportExperimentData.(@solutions)
+    zip_path = ExportExperimentData.()
     send_file(zip_path)
   end
 end
-
-
